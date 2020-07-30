5cc3e6b4941117085cfff39e2ac329feaa67d2f7
goldsteinsveta <svetlana.goldstein@gmail.com>
2020-07-30 16:06:32 +0200

    re-add colors

--- projects/burnr/src/themes/substrate/dark.ts
+++ projects/burnr/src/themes/substrate/dark.ts
@@ -1,14 +1,43 @@
 import { ThemeOptions } from '@material-ui/core/styles';
+import colors from './colors';
 
 const dark: ThemeOptions = {
 	palette: {
 		type: 'dark',
+
+		common: {
+			black: colors.black,
+			white: colors.white,
+		},
 		background: {
-			default: '#172026',
+			paper: colors.dark.main,
+			default: colors.dark.dark,
 		},
 		primary: {
-			main: '#16DB9A',
+			light: colors.cyan.light,
+			main: colors.cyan.main,
+			dark: colors.cyan.dark,
+			contrastText: colors.black,
 		},
+		secondary: {
+			light: colors.pink.light,
+			main: colors.pink.main,
+			dark: colors.pink.dark,
+			contrastText: colors.white,
+		},
+		error: {
+			light: 'rgba(247, 4, 7, 1)',
+			main: 'rgba(235, 4, 7, 1)',
+			dark: 'rgba(197, 5, 8, 1)',
+			contrastText: colors.paper,
+		},
+		text: {
+			primary: colors.white,
+			secondary: colors.light.main,
+			disabled: colors.dark.light,
+			hint: colors.yellow.main,
+		},
+		divider: 'rgba(238, 238, 238, 0.12)',
 	},
 };
 
--- projects/burnr/src/themes/substrate/light.ts
+++ projects/burnr/src/themes/substrate/light.ts
@@ -1,14 +1,43 @@
 import { ThemeOptions } from '@material-ui/core/styles';
+import colors from './colors';
 
 const light: ThemeOptions = {
 	palette: {
 		type: 'light',
+
+		common: {
+			black: colors.black,
+			white: colors.white,
+		},
 		background: {
-			default: '#F7F7F7',
+			paper: colors.light.light,
+			default: colors.white.dark,
 		},
 		primary: {
-			main: '#1E88E5',
+			light: colors.cyan.light,
+			main: colors.cyan.main,
+			dark: colors.cyan.dark,
+			contrastText: colors.black,
 		},
+		secondary: {
+			light: colors.pink.light,
+			main: colors.pink.main,
+			dark: colors.pink.dark,
+			contrastText: colors.white,
+		},
+		error: {
+			light: 'rgba(247, 4, 7, 1)',
+			main: 'rgba(235, 4, 7, 1)',
+			dark: 'rgba(197, 5, 8, 1)',
+			contrastText: colors.paper,
+		},
+		text: {
+			primary: colors.black,
+			secondary: colors.dark.main,
+			disabled: colors.light.dark,
+			hint: colors.yellow.main,
+		},
+		divider: colors.cyan.dark,
 	},
 };
 
