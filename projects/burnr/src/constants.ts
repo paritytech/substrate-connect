30b9a57145c5fc504e335b96a445c0b94ffe0666
Stefanie Doll <stefie.doll@googlemail.com>
2020-07-31 14:21:36 +0200

    Added constants file

---
+++ projects/burnr/src/constants.ts
@@ -0,0 +1,13 @@
+export const endpoints = {
+  'kusama': 'wss://kusama-rpc.polkadot.io/',
+  'polkadotWs': 'wss://rpc.polkadot.io',
+  'westendWs': 'wss://westend-rpc.polkadot.io',
+  'localPolkadotNetwork': 'ws://127.0.0.1:9945',
+  'localHost': 'ws://127.0.0.1:9944'
+};
+
+export const users = {
+  'kusama': 'CzugcapJWD8CEHBYHDeFpVcxfzFBCg57ic72y4ryJfXUnk7',
+  'polkadot': '11uMPbeaEDJhUxzU4ZfWW9VQEsryP9XqFcNRfPdYda6aFWJ',
+  'westend': '12gG5fz9A7k7CgZeis8JesCoZiARDioonHYp5W9Vkwc6nFyB'
+}
