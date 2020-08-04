fbbd93796613099e8e6a83aec7a28d90c7c454f6
Stefanie Doll <stefie.doll@googlemail.com>
2020-08-04 14:22:50 +0200

    Remove @substrate/connect code & connect to local node by default

--- projects/burnr/src/hooks/api/useApiCreate.ts
+++ projects/burnr/src/hooks/api/useApiCreate.ts
@@ -3,28 +3,26 @@
 import { useEffect, useState } from 'react';
 import { ApiPromise, WsProvider } from '@polkadot/api';
 
-import {
-  kusama,
-  LightClient,
-  polkadot,
-  WasmProvider,
-  westend,
-} from '@substrate/connect';
-
 import { endpoints } from '../../constants';
-
 import useIsMountedRef from './useIsMountedRef';
 
-export const PolkadotWASM = './assets/polkadot_cli_bg.wasm';
-const polkadotLocalWs = westend.fromUrl(PolkadotWASM);
-const wasmclient = new WasmProvider(polkadotLocalWs);
-
+/**  This part isn't usable until the issues in the Substrate Light CLient implementation have been fixed **/
+// import {
+//   kusama,
+//   LightClient,
+//   polkadot,
+//   WasmProvider,
+//   westend,
+// } from '@substrate/connect';
+//
+// export const PolkadotWASM = './assets/polkadot_cli_bg.wasm';
+// const polkadotLocalWs = westend.fromUrl(PolkadotWASM);
+// const wasmclient = new WasmProvider(polkadotLocalWs);
 // const provider = new WasmProvider(polkadotLocalWs);
 // const rpc = new Rpc(provider);
-
-
-console.log('wasmclient', wasmclient)
-console.log('polkadotLocalWs', polkadotLocalWs)
+//
+// console.log('wasmclient', wasmclient)
+// console.log('polkadotLocalWs', polkadotLocalWs)
 
 
 export default function useApiCreate (): ApiPromise | null {
@@ -34,13 +32,16 @@ export default function useApiCreate (): ApiPromise | null {
   useEffect((): void => {
     ApiPromise
       .create({
-        provider: new WsProvider(endpoints.westend),
+        provider: new WsProvider(endpoints.localHost),
         types: {}
       })
       .then((api): void => {
+        console.log(`Connected to local chain at ${endpoints.localHost}`)
         mountedRef.current && setApi(api);
       })
-      .catch(console.error);
+      .catch((): void => {
+        console.error
+      });
   }, []);
 
   return api;
