a29b69aa3f54c72b50d478fa1f76b1f4f8eb5546
Stefanie Doll <stefie.doll@googlemail.com>
2020-10-15 17:49:23 +0200

    Update rpc-provider dependency

--- projects/burnr/src/constants.ts
+++ projects/burnr/src/constants.ts
@@ -12,11 +12,8 @@ import { LazyProvider } from './utils/types';
  * Temporary hard-coded work around to test Wasm Light client 
  * until @substrate/connect is properly implemented
  */
+import { polkadotLocal, WasmProvider } from '../assets/wasm_client';
 
-import { polkadotLocal } from '../assets/wasm_client';
-// import { initClient } from '../assets/wasm_client/initClient';
-// const WasmClientPath = '../assets/wasm_client/polkadot_cli_bg.wasm';
-// const WasmClient: LightClient = initClient(WasmClientPath)
 
 export const endpoints = {
   'kusama': 'wss://kusama-rpc.polkadot.io/',
@@ -33,20 +30,18 @@ export const users = {
 }
 
 export const JS_WASM_PROVIDERS: Record<string, LazyProvider> = {
-  // 'Polkadot-Local-WasmProvider': {
-  //   description: 'Local WASM light client for polkadot-local network',
-  //   id: 'Polkadot-Local-WasmProvider',
-  //   network: 'Local Polkadot Network',
-  //   node: 'light',
-  //   source: 'browser',
-  //   endpoint: 'Light client running in Browser',
-  //   client: 'Wasm light',
-  //   start: (): Promise<ProviderInterface> =>
-  //     Promise.resolve(new WasmProvider(WasmClient)),
-  //   // start: (): Promise<ProviderInterface> =>
-  //   //   Promise.resolve(polkadotLocal()),
-  //   transport: 'WasmProvider',
-  // }
+  'Polkadot-Local-WasmProvider': {
+    description: 'Local WASM light client for polkadot-local network',
+    id: 'Polkadot-Local-WasmProvider',
+    network: 'Local Polkadot Network',
+    node: 'light',
+    source: 'browser',
+    endpoint: 'Light client running in Browser',
+    client: 'Wasm light',
+    start: (): Promise<ProviderInterface> =>
+      Promise.resolve(new WasmProvider(polkadotLocal())),
+    transport: 'WasmProvider',
+  }
   // 'Polkadot-Wasm-Light-Node': {
   //   description: 'Local WASM light client for Polkadot',
   //   id: 'Polkadot-WasmProvider',
