3dddf09bab9262783a7bee87c9ade450d807aa26
Raoul Millais <raoul@raoulmillais.com>
2021-01-20 11:31:19 +0000

    Test sequential message ids on send
    - Refactor test setup and add notes for clarity

--- packages/smoldot-provider/src/send.test.ts
+++ packages/smoldot-provider/src/send.test.ts
@@ -5,12 +5,17 @@ import { SmoldotClient, SmoldotOptions } from 'smoldot';
 
 type RpcResponder = (message: string) => string;
 
+// Mimics the behaviour of the WASM light client by deferring a call to 
+// `json_rpc_callback` after it is called which returns the response
+// returned by the supplied `responder`.
 const fakeRpcSend = (options: SmoldotOptions, responder: RpcResponder) => {
   return (rpc: string) => {
     process.nextTick(() => options.json_rpc_callback(responder(rpc)));
   };
 }
 
+// Creates a fake smoldot. Calls to send use `fakeRpcSend` to mimic the light
+// client behaviour
 const mockSmoldot = (responder: RpcResponder) => {
     return {
         start: async (options: SmoldotOptions): Promise<SmoldotClient> => {
@@ -23,6 +28,8 @@ const mockSmoldot = (responder: RpcResponder) => {
     };
 };
 
+// Creates a spying `smoldot` that records calls to `json_rpc_send` in `rpcSpy`
+// and then uses `fakeRpcSend` to mimic the light client behaviour.
 const smoldotSpy = (responder: RpcResponder, rpcSpy: any) => {
     return {
         start: async (options: SmoldotOptions): Promise<SmoldotClient> => {
@@ -37,6 +44,17 @@ const smoldotSpy = (responder: RpcResponder, rpcSpy: any) => {
         }
     };
 };
+
+// Creates an `RpcResponder` with a queue of mock reponses to respond with.
+const respondWith = (jsonResponses: string[]) => {
+  return (_: string) => {
+    const mockResponse = jsonResponses.shift();
+    if (!mockResponse) { 
+      throw new Error("json_rpc_callback was called but there are no mock responses left to respond with"); 
+    }
+    return mockResponse;
+  };
+}
 
 test('connect resolves and emits', async t => {
     const echoSmoldot = mockSmoldot(x => x);
@@ -53,8 +71,8 @@ test('connect resolves and emits', async t => {
 // response has no method field
 // handler was not registered by `subscribe` I.E. has no associated `subscription`
 test('awaiting send returns message result', async t => {
-    const replyJson =  '{ "id": 1, "jsonrpc": "2.0", "result": "success" }';
-    const ms = mockSmoldot(_ => replyJson);
+    const mockReponses =  ['{ "id": 1, "jsonrpc": "2.0", "result": "success" }'];
+    const ms = mockSmoldot(respondWith(mockResponses));
     const provider = new SmoldotProvider("", ms);
 
     await provider.connect();
@@ -63,10 +81,10 @@ test('awaiting send returns message result', async t => {
 });
 
 test('send formats JSON RPC request correctly', async t => {
-    // we don't really care what the reply is for this test
-    const replyJson =  '{ "id": 1, "jsonrpc": "2.0", "result": "success" }';
+    // don't really care what the reply is
+    const mockResponses =  ['{ "id": 1, "jsonrpc": "2.0", "result": "success" }'];
     const rpcSend = sinon.spy();
-    const ss = smoldotSpy(_ => replyJson, rpcSend);
+    const ss = smoldotSpy(respondWith(mockResponses), rpcSend);
     const provider = new SmoldotProvider("", ss);
 
     await provider.connect();
@@ -76,3 +94,25 @@ test('send formats JSON RPC request correctly', async t => {
     t.is(rpcJson, '{"id":1,"jsonrpc":"2.0","method":"hello","params":["world"]}');
 });
 
+test('sending twice uses new id', async t => {
+    // don't really care what the replies are
+    const mockResponses =  [ 
+      '{ "id": 1, "jsonrpc": "2.0", "result": "success" }',
+      '{ "id": 2, "jsonrpc": "2.0", "result": "success" }'
+    ];
+    const rpcSend = sinon.spy();
+    const ss = smoldotSpy(respondWith(mockResponses), rpcSend);
+    const provider = new SmoldotProvider("", ss);
+
+    await provider.connect();
+    await provider.send('hello', [ 'world' ]);
+    await provider.send('hello', [ 'world' ]);
+
+    t.true(rpcSend.called);
+    t.true(rpcSend.calledTwice);
+
+    const rpcJson1 = rpcSend.firstCall.firstArg;
+    t.is(rpcJson1, '{"id":1,"jsonrpc":"2.0","method":"hello","params":["world"]}');
+    const rpcJson2 = rpcSend.secondCall.firstArg;
+    t.is(rpcJson2, '{"id":2,"jsonrpc":"2.0","method":"hello","params":["world"]}');
+});
