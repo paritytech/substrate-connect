cec5b9bf5f10a77d45fe352ccda0306977b70b6b
Raoul Millais <raoul@raoulmillais.com>
2021-01-21 11:05:22 +0000

    Add constants and queries to example
    - Use the polkadot-js logger
    - Queries are broken because of the unimplemented database callback

--- packages/smoldot-provider/src/examples/index.ts
+++ packages/smoldot-provider/src/examples/index.ts
@@ -1,13 +1,28 @@
 import { ApiPromise } from '@polkadot/api';
 import SmoldotProvider from '../';
 import westend_specs from './westend_specs';
+import {logger} from '@polkadot/util';
 
-console.log('Loading chain spec');
+const l = logger('smoldot-provider');
+
+l.log('Creating a smoldot client');
 const chainSpec = JSON.stringify(westend_specs());
-console.log('Creating provider');
 const provider = new SmoldotProvider(chainSpec);
-console.log('Connecting provider');
 await provider.connect();
-console.log('Creating API wrapper');
 const api = await ApiPromise.create({ provider });
-console.log('Done');
+l.log('API is ready');
+
+/**
+ * API Constants
+ */ 
+l.log('genesis hash: ', api.genesisHash.toHex());
+l.log('epoch duration: ', api.consts.babe.epochDuration.toNumber());
+l.log('balance required to prevent an account from being deleted: ' , api.consts.balances.existentialDeposit.toHuman());
+
+/**
+ * Basic Queries
+ */ 
+const ADDR = '5FHyraDcRvSYCoSrhe8LiBLdKmuL9ptZ5tEtAtqfKfeHxA4y';
+const now = await api.query.timestamp.now();
+const { nonce, data: balance } = await api.query.system.account(ADDR);
+l.log(`balance of ${balance.free} and a nonce of ${nonce}`);
