e5a9abd2c2897adebed924be6ef9f015be692546
Raoul Millais <raoul@raoulmillais.com>
2021-01-21 17:28:27 +0000

    Run examples separately from unit tests
    - Move ava config into separate files
    - Add a yarn examples script

---
+++ packages/smoldot-provider/ava.config.js
@@ -0,0 +1,21 @@
+// Typings arent available for next gen config yet
+export default {
+  nonSemVerExperiments: {
+    nextGenConfig: true,
+    configurableModuleFormat: true
+  },
+  files: [
+    'src/**/*.test.ts',
+    '!src/examples'
+  ],
+  extensions: {
+    ts: "module"
+  },
+  nodeArguments: [
+    "--loader=ts-node/esm",
+    "--experimental-specifier-resolution=node"
+  ],
+  require: [
+    "ts-node/register"
+  ]
+};
---
+++ packages/smoldot-provider/ava.examples.config.js
@@ -0,0 +1,21 @@
+// Typings arent available for next gen config yet
+// Until next gen config is more stable we have to duplicate all the config :(
+export default {
+  nonSemVerExperiments: {
+    nextGenConfig: true,
+    configurableModuleFormat: true
+  },
+  files: [
+    'src/examples/**/*.test.ts'
+  ],
+  extensions: {
+    ts: "module"
+  },
+  nodeArguments: [
+    "--loader=ts-node/esm",
+    "--experimental-specifier-resolution=node"
+  ],
+  require: [
+    "ts-node/register"
+  ]
+};
