682744b6567d0bf174015a8ac86417b359af4a00
Stefanie Doll <stefie.doll@googlemail.com>
2021-01-29 18:18:40 +0100

    Switch to React / tsx

--- projects/burnr/index.ts
+++
@@ -1,1 +0,0 @@
-console.log('Hello Parcel Project 2');
---
+++ projects/burnr/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <p>Hello</p>,
+  document.getElementById('root'),
+)
+
+console.log('Hello Parcel Project 2');
