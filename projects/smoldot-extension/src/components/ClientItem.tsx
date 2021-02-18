65b9380de0fdb9faf1e5af5e1b68e1b4157f51c6
goldsteinsveta <sveta@parity.io>
2021-02-18 18:40:25 +0100

    ClientItem Menu

--- projects/smoldot-extension/src/components/ClientItem.tsx
+++ projects/smoldot-extension/src/components/ClientItem.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { makeStyles, Theme, Typography, Box, ButtonBase, ButtonGroup, Grid, Menu, MenuItem, Tooltip } from '@material-ui/core';
+import { makeStyles, Popover, Theme, Typography, Box, ButtonBase, Button, ButtonGroup, Grid, Menu, MenuItem, Tooltip, Divider } from '@material-ui/core';
 import { grey } from '@material-ui/core/colors';
+import { ExpandMore, SystemUpdateAlt, InsertDriveFile, Publish } from '@material-ui/icons';
 import { IconWeb3 } from '../components';
-import { ExpandMore, SystemUpdateAlt } from '@material-ui/icons';
+import { InputButton, InputText, InputWrap } from './Inputs';
 
 interface Props {
 	isKnown?: boolean
@@ -29,44 +30,81 @@ const useStyles = makeStyles<Theme, Props>(theme => ({
 		backgroundColor: grey[100],
 		borderRadius: '50%',
 	},
+  // TODO Popup to use the same component
+	popoverRoot: {
+    '& .MuiPopover-paper': {
+      width: theme.spacing(45),
+      paddingTop: theme.spacing(1.5),
+      boxShadow: `0px 4px 12px rgb(0 0 0 / 15%)`,
+    },
+    '& .MuiPopover-paper > *:not(hr)': {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+      '&:not(button)': {
+        marginLeft: theme.spacing(2),
+        marginRight: theme.spacing(2),
+      }
+    }
+  },
 }));
 
 const ClientMenu: React.FunctionComponent<Props> = ({isKnown}) => {
+  const classes = useStyles({isKnown});
 	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
   };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <>
-    <ButtonGroup disableRipple>
-      { !isKnown &&
-      <Tooltip
-        title='Save <network> chainspec to extension'
-        placement='top'
-        arrow
-      >
-        <ButtonBase>
-          <SystemUpdateAlt fontSize='small' />
+      <ButtonGroup disableRipple>
+        { !isKnown &&
+        <Tooltip
+          title='Save <network> chainspec to extension'
+          placement='top'
+          arrow
+        >
+          <ButtonBase>
+            <SystemUpdateAlt fontSize='small' />
+          </ButtonBase>
+        </Tooltip>
+        }
+        <ButtonBase onClick={handleClick}>
+          <ExpandMore fontSize='small' />
         </ButtonBase>
-      </Tooltip>
-      }
-      <ButtonBase onClick={handleClick}>
-				<ExpandMore fontSize='small' />
-			</ButtonBase>
-      </ButtonGroup>
+        </ButtonGroup>
 
-      <Menu
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={() => setAnchorEl(null)}
-      >
-				<Typography variant='overline'>Chainspec</Typography>
-        <MenuItem>Input: File</MenuItem>
-        <MenuItem>Input: DisplayName</MenuItem>
-      </Menu>
+        <Popover
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+          className={classes.popoverRoot}
+        >
+
+          <Typography variant='overline'>Chainspec</Typography>
+          <InputWrap>
+            <InputText readOnly defaultValue='chainspec.json'/>
+            <Divider orientation='vertical' flexItem/>
+            <InputButton>
+              <InsertDriveFile fontSize='small'/>
+            </InputButton>
+            <Divider orientation='vertical' flexItem/>
+            <InputButton>
+              <Publish fontSize='small'/>
+            </InputButton>
+          </InputWrap>
+          <InputWrap>
+            <InputText defaultValue='<networkName>'/>
+          </InputWrap>
+          <Divider/>
+          <Button onClick={handleClose} fullWidth>Cancel</Button>
+
+        </Popover>
     </>
   );
 }
