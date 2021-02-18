d01b0d03b82dd41e6b9b7b0fd048adb9225262e3
goldsteinsveta <sveta@parity.io>
2021-02-18 12:15:52 +0100

    ClientTypeTitle

--- projects/smoldot-extension/src/containers/Options.tsx
+++ projects/smoldot-extension/src/containers/Options.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import { Box, createMuiTheme, ThemeProvider, Typography } from '@material-ui/core';
 import { light, IconWeb3, ClientSearch, Logo } from '../components/';
 import GlobalFonts from '../fonts/fonts';
+
+const ClientTypeTitle: React.FunctionComponent = ({children}) => (
+	<Box marginY={4}>
+		<Typography variant='overline'>
+			{children}
+		</Typography>
+	</Box>
+)
 
 const Options: React.FunctionComponent = () => {
 	const appliedTheme = createMuiTheme(light);
@@ -11,6 +19,8 @@ const Options: React.FunctionComponent = () => {
 			<GlobalFonts />
 			<Logo />
 			<ClientSearch />
+			<ClientTypeTitle children='Local chainspecs'/>
+			<ClientTypeTitle children='Chainspecs from uApps'/>
 			<IconWeb3>polkadot kusama</IconWeb3>
 			<div>Options Main page</div>
 		</ThemeProvider>
