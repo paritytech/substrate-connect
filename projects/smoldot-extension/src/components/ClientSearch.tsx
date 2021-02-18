864582e752359b3e8b687a038e85da42ac36c689
goldsteinsveta <sveta@parity.io>
2021-02-18 18:39:29 +0100

    styled recurring components

--- projects/smoldot-extension/src/components/ClientSearch.tsx
+++ projects/smoldot-extension/src/components/ClientSearch.tsx
@@ -1,48 +1,18 @@
 import React from 'react';
-import { makeStyles, Grid, InputBase, Divider, ButtonBase } from '@material-ui/core';
-import { grey } from '@material-ui/core/colors';
+import { Divider } from '@material-ui/core';
 import { SystemUpdateAlt } from '@material-ui/icons';
-
-const useStyles = makeStyles(theme => ({
-	root: {
-		backgroundColor: grey[100],
-		borderRadius: theme.spacing(0.5),
-		border: `1px solid ${grey[200]}`,
-	},
-	inputRoot: {
-		'& input': {
-			padding: theme.spacing(1),
-		},
-	},
-	buttonRoot: {
-		borderTopRightRadius: theme.spacing(0.5),
-		borderBottomRightRadius: theme.spacing(0.5),
-		backgroundColor: theme.palette.background.default,
-		paddingLeft: theme.spacing(1.2),
-		paddingRight: theme.spacing(1.2),
-	},
-}));
+import { InputButton, InputText, InputWrap } from './Inputs';
 
 const ClientSearch: React.FunctionComponent = () => {
-	const classes = useStyles();
-
-	return (
-    <Grid
-			container
-			wrap='nowrap'
-			className={classes.root}
-		>
-      <InputBase
-				fullWidth
-				placeholder='Search by network, uApp or url'
-				className={classes.inputRoot}
-			/>
-			<Divider orientation='vertical' flexItem/>
-      <ButtonBase className={classes.buttonRoot}>
+  return (
+    <InputWrap>
+      <InputText placeholder='Search by network, uApp or url' />
+      <Divider orientation='vertical' flexItem/>
+      <InputButton>
         <SystemUpdateAlt fontSize='small' />
-      </ButtonBase>
-    </Grid>
-	);
+      </InputButton>
+    </InputWrap>
+  );
 };
 
 export default ClientSearch;
---
+++ projects/smoldot-extension/src/components/Inputs.tsx
@@ -0,0 +1,63 @@
+import { InputBase, ButtonBase, Box } from '@material-ui/core';
+import { withStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { grey } from '@material-ui/core/colors';
+
+/**
+ * `InputWrap` is used to wrap `InputText` and `InputButton`
+ *
+ * API:
+ *
+ * - [Box](https://material-ui.com/components/box/)
+ * 
+ */
+export const InputWrap = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'nowrap',
+      backgroundColor: grey[100],
+      borderRadius: theme.spacing(0.5),
+      border: `1px solid ${grey[200]}`,
+    },
+  })
+)(Box);
+
+
+/**
+ * `InputText` is used inside `InputWrap` next to `InputButton`
+ *
+ * API:
+ *
+ * - [InputBase API](https://material-ui.com/api/input-base/)
+ * 
+ */
+export const InputText = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      '& input': {
+        padding: theme.spacing(1),
+      },
+    },
+  })
+)(InputBase);
+
+/**
+ * `InputButton` is used inside `InputWrap` next to `InputText`
+ *
+ * API:
+ *
+ * - [ButtonBase API](https://material-ui.com/api/button-base/)
+ * 
+ */
+export const InputButton = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      borderTopRightRadius: theme.spacing(0.5),
+      borderBottomRightRadius: theme.spacing(0.5),
+      backgroundColor: theme.palette.background.default,
+      paddingLeft: theme.spacing(1.2),
+      paddingRight: theme.spacing(1.2),
+    },
+  })
+)(ButtonBase);
