aed2b01cac79e0cdb06668bb029886cb5b352ff4
Raoul Millais <raoul@raoulmillais.com>
2021-02-23 10:03:22 +0000

    Remove background from legact smoldot-extension folder

--- projects/smoldot-extension/src/background/index.ts
+++
@@ -1,8 +0,0 @@
-import { ConnectionManager } from './ConnectionManager';
-import westend from '../assets/westend.json';
-
-const manager = new ConnectionManager();
-
-chrome.runtime.onStartup.addListener(async () => {
-  await manager.addSmoldot('westend', JSON.stringify(westend));
-});
