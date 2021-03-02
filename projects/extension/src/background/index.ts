7b7d8b46889e082ce4772a331298d24bb1ade5c0
Nikos Kontakis <wirednkod@gmail.com>
2021-03-02 10:14:56 +0200

    revert normal wss westend json

--- projects/extension/src/background/index.ts
+++ projects/extension/src/background/index.ts
@@ -1,10 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { ConnectionManager } from './ConnectionManager';
-// import westendWss from '../assets/westend-wss.json';
-import westendWs from '../assets/westend-ws.json';
+
+// secure ws - test chainspec
+import westend from '../assets/westend-wss.json';
+// Non secure ws - test chainspec
+// import westend from '../assets/westend-ws.json';
 const manager = new ConnectionManager();
 
 chrome.runtime.onConnect.addListener(() => {
-  manager.addSmoldot('westend', JSON.stringify(westendWs))
+  manager.addSmoldot('westend', JSON.stringify(westend))
     .catch((e) => { console.error(e); });
 });
