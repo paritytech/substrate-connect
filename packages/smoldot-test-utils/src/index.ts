dff983f76bf7d22030fb393ee4edd32ad9a53088
Nikos Kontakis <wirednkod@gmail.com>
2021-04-06 13:50:12 +0300

    upgrade bits and pieces

--- packages/smoldot-test-utils/src/index.ts
+++ packages/smoldot-test-utils/src/index.ts
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { SmoldotClient, SmoldotOptions } from 'smoldot';
 
 type RpcResponder = (request: string) => string;
@@ -58,15 +64,17 @@ export const devChainHealthResponder = (requestJSON: string) => {
 const fakeRpcSend = (options: SmoldotOptions, responder: RpcResponder, healthResponder: RpcResponder) => {
   return (rpcRequest: string) => {
     process.nextTick(() => {
-      if (/system_health/.test(rpcRequest)) {
-        options.json_rpc_callback(healthResponder(rpcRequest));
-        return;
-      }
+      if (options && options.json_rpc_callback) {
+        if (/system_health/.test(rpcRequest)) {
+          options.json_rpc_callback(healthResponder(rpcRequest));
+          return;
+        }
 
-      // non-health reponse
-      options.json_rpc_callback(responder(rpcRequest))
-      if (/(?<!un)[sS]ubscribe/.test(rpcRequest)) {
+        // non-health reponse
         options.json_rpc_callback(responder(rpcRequest))
+        if (/(?<!un)[sS]ubscribe/.test(rpcRequest)) {
+          options.json_rpc_callback(responder(rpcRequest))
+        }
       }
     });
   };
