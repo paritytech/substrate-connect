817d1cd6f229fd5142f87d21e2eb398a8bf3ce6d
Raoul Millais <raoul@raoulmillais.com>
2021-04-08 10:07:49 +0100

    Convert EP tests to `test` syntax

--- packages/connect/src/ExtensionProvider/ExtensionProvider.test.ts
+++ packages/connect/src/ExtensionProvider/ExtensionProvider.test.ts
@@ -1,38 +1,34 @@
 import {jest} from '@jest/globals'
 import { ExtensionProvider } from './ExtensionProvider';
 
-describe('ExtensionProvider', () => {
+beforeEach(() => {
+  window.postMessage = jest.fn();
+});
 
-  beforeEach(() => {
-    window.postMessage = jest.fn();
-  });
+test('constructor sets properties', async () => {
+  const ep = new ExtensionProvider('test', 'kusama');
+  expect(ep.name).toBe('test');
+  expect(ep.chainName).toBe('kusama');
+});
 
-  it('constructor sets properties', async () => {
-    const ep = new ExtensionProvider('test', 'kusama');
-    expect(ep.name).toBe('test');
-    expect(ep.chainName).toBe('kusama');
-  });
-
-  it('connect sends init message and emits connected', async () => {
-    const ep = new ExtensionProvider('test', 'test-chain');
-    const emitted = jest.fn();
-    ep.on('connected', emitted);
-    await ep.connect();
+test('connect sends init message and emits connected', async () => {
+  const ep = new ExtensionProvider('test', 'test-chain');
+  const emitted = jest.fn();
+  ep.on('connected', emitted);
+  await ep.connect();
 
 
-    const expectedMessage = {
-      appName: 'test',
-      chainName: 'test-chain',
-      message: {
-        type: 'associate',
-        payload: 'test-chain'
-      },
-      origin: 'extension-provider'
-    };
-    expect(window.postMessage).toHaveBeenCalledTimes(1);
-    expect(window.postMessage).toHaveBeenCalledWith(expectedMessage, '*');
-    expect(ep.isConnected).toBe(true);
-    expect(emitted).toHaveBeenCalledTimes(1);
-  });
-
+  const expectedMessage = {
+    appName: 'test',
+    chainName: 'test-chain',
+    message: {
+      type: 'associate',
+      payload: 'test-chain'
+    },
+    origin: 'extension-provider'
+  };
+  expect(window.postMessage).toHaveBeenCalledTimes(1);
+  expect(window.postMessage).toHaveBeenCalledWith(expectedMessage, '*');
+  expect(ep.isConnected).toBe(true);
+  expect(emitted).toHaveBeenCalledTimes(1);
 });
