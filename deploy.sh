6e8df1c08d29c1d29c0b4ec8f1053faae15be57d
Nikos Kontakis <wirednkod@gmail.com>
2021-04-08 11:33:02 +0300

    minor alternation on build and deploy scripts in order to reduce unnecessary code

--- deploy.sh
+++ deploy.sh
@@ -13,47 +13,30 @@ readonly -f die
 
 initDirs() {
   rm -rf ./docs/*
-  mkdir ./docs/burnr
-  mkdir ./docs/smoldot-browser-demo
+  mkdir -p ./docs/burnr
+  mkdir -p ./docs/smoldot-browser-demo
 }
 
 burnr() {
-  cp -r ./projects/burnr/dist/* ./docs/burnr
-  sed 's/href="/href="./g' ./docs/burnr/index.html > ./docs/burnr/tmp
-  sed 's/src="/src="./g' ./docs/burnr/tmp > ./docs/burnr/tmp2
-  mv ./docs/burnr/tmp2 ./docs/burnr/index.html
 }
 
 smoldotBrowserDemo() {
-  cp -r ./projects/smoldot-browser-demo/dist/* ./docs/smoldot-browser-demo
-  sed 's/href="/href="./g' ./docs/smoldot-browser-demo/index.html > ./docs/smoldot-browser-demo/tmp
-  sed 's/src="/src="./g' ./docs/smoldot-browser-demo/tmp > ./docs/smoldot-browser-demo/tmp2
-  mv ./docs/smoldot-browser-demo/tmp2 ./docs/smoldot-browser-demo/index.html
 }
 
 landingPage() {
   cp ./README.md ./docs/.
 }
 
-cleanup() {
-  rm -rf ./docs/burnr/tmp*
-  rm -rf ./docs/smoldot-browser-demo/tmp*
-}
-
 deployGhPages() {
   echo "Init demo gh-pages process..."
   initDirs
-  echo "Create burnr wallet demo."
-  burnr
-  echo "Create Smoldot browser demo."
-  smoldotBrowserDemo
-  echo "Create landing page"
+  echo "Place burnr wallet demo's files."
+  cp -r ./projects/burnr/dist/* ./docs/burnr
+  echo "Place Smoldot browser demo's files."
+  cp -r ./projects/smoldot-browser-demo/dist/* ./docs/smoldot-browser-demo
+  echo "Place landing page's files."
   landingPage
-  echo "Cleanup directories"
-  cleanup
-  echo "Deployed to gh-pages"
-}
+ }
 
 deployGhPages
-
-# deploy burnr wallet and smoldot browser demo to ipfs
+echo "Deployed to gh-pages"
