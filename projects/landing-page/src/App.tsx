b7e8afa4f77aab3e926c3ac7533237b556309160
goldsteinsveta <sveta@parity.io>
2021-04-13 18:35:51 +0200

    theme provider

--- projects/landing-page/src/App.tsx
+++ projects/landing-page/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Container, CssBaseline, Typography, Paper, Grid } from '@material-ui/core';
+import { Container, CssBaseline, Typography, Paper, Grid, ThemeProvider, createMuiTheme } from '@material-ui/core';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import { Logo } from './components';
+import { Logo, theme } from './components';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -19,30 +19,32 @@ const useStyles = makeStyles((theme: Theme) =>
 const App = () => {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <Container fixed>
-        <Grid container spacing={3}>
-          <Grid item xs={9}>
-            <Paper className={classes.paper}>xs=9</Paper>
+    <ThemeProvider theme={createMuiTheme(theme)}>
+      <div className={classes.root}>
+        <CssBaseline />
+        <Container fixed>
+          <Grid container spacing={3}>
+            <Grid item xs={9}>
+              <Paper className={classes.paper}>xs=9</Paper>
+            </Grid>
+            <Grid item xs={3}>
+              <div className={classes.paper}><Logo /></div>
+              <div className={classes.paper}>
+                <ul>
+                  <li>Substrate-based chain</li>
+                  <li>Light Clients</li>
+                  <li>Supported Networks</li>
+                  <li>Getting Starter</li>
+                  <li>Projects</li>
+                  <li>Playground</li>
+                  <li>Github Repository</li>
+                </ul>
+              </div>
+            </Grid>
           </Grid>
-          <Grid item xs={3}>
-            <div className={classes.paper}><Logo /></div>
-            <div className={classes.paper}>
-              <ul>
-                <li>Substrate-based chain</li>
-                <li>Light Clients</li>
-                <li>Supported Networks</li>
-                <li>Getting Starter</li>
-                <li>Projects</li>
-                <li>Playground</li>
-                <li>Github Repository</li>
-              </ul>
-            </div>
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
+        </Container>
+      </div>
+    </ThemeProvider>
   );
 }
 
