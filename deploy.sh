d9cedb78d8b4cd2cb10c0a689e4ecaa7c56c4c6e
Nikos Kontakis <wirednkod@gmail.com>
2021-04-23 12:09:02 +0300

    Minor correction on deploy script; Fixes #199

--- deploy.sh
+++ deploy.sh
@@ -38,8 +38,8 @@ deployGhPages() {
   cp -r ./projects/smoldot-browser-demo/dist/* ./$directory/smoldot-browser-demo/.
   echo "Place Multiple network demo's files."
   cp -r ./projects/multiple-network-demo/dist/* ./$directory/multiple-network-demo/.
-  echo "Place Multiple extension's zip."
-  cp -r ./projects/extension/dist/substrate-connect.zip ./$directory/extension/.
+  echo "Place Substrate-connect extension's zip."
+  cp -r ./projects/extension/dist/substrate-connect.zip ./$directory/extension/substrate-connect.zip
   echo "Place landing page's files."
   landingPage
  }
