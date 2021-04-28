28cae41d45f549427444959d83dca3dcc81c9d0e
Nikos Kontakis <wirednkod@gmail.com>
2021-04-28 22:39:06 +0300

    Fix race condition

--- projects/extension/src/page.ts
+++ projects/extension/src/page.ts
@@ -3,12 +3,14 @@
 const pkg_name = process.env.PKG_NAME as string;
 const version = process.env.PKG_VERSION as string;
 
-injectExtension();
-
-function injectExtension() {
+const injectExtension = () => {
   const s = document.createElement('span');
   s.id = 'substrateExtension'
   s.setAttribute('name', pkg_name);
   s.setAttribute('version', version);
   document.body.appendChild(s);
 }
+
+window.onload = () => {
+  injectExtension();
+};
