2264f0ecac9f8465064ead4bab7849486d6334cb
goldsteinsveta <sveta@parity.io>
2021-05-19 15:44:34 +0200

    refactor Burnr UI - ReceiveFundsForm

--- projects/burnr/src/components/NavTabs.tsx
+++ projects/burnr/src/components/NavTabs.tsx
@@ -18,113 +18,109 @@ interface TabPanelProps {
 }
 
 const useStyles = makeStyles((theme: Theme) => ({
-	root: {
-		height:'calc(100vh - 265px)',
-		borderTopRightRadius: 0,
-		borderTopLeftRadius: 0,
+  root: {
+    minHeight:'calc(100vh - 265px)',
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 0,
 
-		[theme.breakpoints.down('sm')]: {
-			height:'calc(100vh - 320px)',
-		},
-	},
-	tabBurn: {
-		'& svg, & .MuiTab-wrapper': {
-			color: theme.palette.error.main
-		}
-	},
-	rootTabs: {
-		'& .MuiTab-root': {
-			minHeight: theme.spacing(8),
-			padding: 0,
-			...theme.typography.overline,
-			lineHeight: 1
-		}
-	}
+    [theme.breakpoints.down('sm')]: {
+      minHeight:'calc(100vh - 320px)',
+    },
+  },
+  tabBurn: {
+    '& svg, & .MuiTab-wrapper': {
+      color: theme.palette.error.main
+    }
+  },
+  rootTabs: {
+    '& .MuiTab-root': {
+      minHeight: theme.spacing(8),
+      padding: 0,
+      ...theme.typography.overline,
+      lineHeight: 1
+    }
+  }
 }));
 
 const TabPanel: React.FunctionComponent<TabPanelProps> = ({ children, value, index, ...props }: TabPanelProps) => {
-	return (
-		<div
-			hidden={value !== index}
-			id={`tabpanel-${index}`}
-			{...props}
-		>
-			{value === index && (
-				<Box p={2}>
-					{children}
-				</Box>
-			)}
-		</div>
-	);
+  return (
+    <div
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      {...props}
+    >
+      {value === index && (
+        <Box p={2}>
+          {children}
+        </Box>
+      )}
+    </div>
+  );
 };
 
 const NavTabs: React.FunctionComponent = () => {
-	const classes = useStyles();
-	const [value, setValue] = React.useState(1);
+  const classes = useStyles();
+  const [value, setValue] = React.useState(1);
 
-	const api = useApi();
-	const [endpoint] = useLocalStorage('endpoint');
-	const minEndpoint = endpoint?.split('-')[0]?.toLowerCase();
-	const { account, setCurrentAccount } = useContext(AccountContext);
-	const balance = useBalance(account.userAddress);
+  const api = useApi();
+  const [endpoint] = useLocalStorage('endpoint');
+  const minEndpoint = endpoint?.split('-')[0]?.toLowerCase();
+  const { account, setCurrentAccount } = useContext(AccountContext);
+  const balance = useBalance(account.userAddress);
 
-	const handleChange = (event: React.ChangeEvent<unknown>, newValue: number) => {
-		if (newValue === 0) {
-			if (!balance[2] && !window.confirm(`Burn keys from account with ${balance[0].toString()} ${api.registry.chainTokens.toString()}?`)) {
-				return
-			}
-			localStorage.removeItem(minEndpoint);
-			const userTmp = createLocalStorageAccount();
-			setCurrentAccount(userTmp);
-			return
-		}
-		setValue(newValue);
-	};
+  const handleChange = (event: React.ChangeEvent<unknown>, newValue: number) => {
+    if (newValue === 0) {
+      if (!balance[2] && !window.confirm(`Burn keys from account with ${balance[0].toString()} ${api.registry.chainTokens.toString()}?`)) {
+        return
+      }
+      localStorage.removeItem(minEndpoint);
+      const userTmp = createLocalStorageAccount();
+      setCurrentAccount(userTmp);
+      return
+    }
+    setValue(newValue);
+  };
 
-	return (
-		<>
-			<Paper square>
-				<Tabs
-					value={value}
-					onChange={handleChange}
-					variant='fullWidth'
-					className={classes.rootTabs}
-				>
-					<Tab label="Burn Account" icon={<WhatshotSharpIcon fontSize='small'/>} className={classes.tabBurn}/>
-					<Tab label="Receipts" icon={<SwapHorizSharpIcon fontSize='small'/>} />
-					<Tab label="Send" icon={<CallMadeSharpIcon fontSize='small'/>} />
-					<Tab label="Receive" icon={<CallReceivedSharpIcon fontSize='small'/>} />
-				</Tabs>
-			</Paper>
+  return (
+    <>
+      <Paper square>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant='fullWidth'
+          className={classes.rootTabs}
+        >
+          <Tab label="Burn Account" icon={<WhatshotSharpIcon fontSize='small'/>} className={classes.tabBurn}/>
+          <Tab label="Receipts" icon={<SwapHorizSharpIcon fontSize='small'/>} />
+          <Tab label="Send" icon={<CallMadeSharpIcon fontSize='small'/>} />
+          <Tab label="Receive" icon={<CallReceivedSharpIcon fontSize='small'/>} />
+        </Tabs>
+      </Paper>
 
-			<BurnrDivider />
+      <BurnrDivider />
 
-			<Paper className={classes.root}>
-				<TabPanel value={value} index={1}>
-					<Typography variant='h2'>
-						Transaction History
-					</Typography>
-					<HistoryTable />
-				</TabPanel>
-				<TabPanel value={value} index={2}>
-					<Typography variant='h2'>
-						Send Funds
-					</Typography>
-					<SendFundsForm />
-				</TabPanel>
-				<TabPanel value={value} index={3}>
-					<Typography variant='h2'>
-						Receive Funds
-					</Typography>
-					<ReceiveFundsForm />
-					{/*  TO-DO:
-						Need to create the <ReceiveFundsFrom />
-						Needs some more brainstorming/research on what will be put here
-					*/}
-				</TabPanel>
-			</Paper>
-		</>
-	);
+      <Paper className={classes.root}>
+        <TabPanel value={value} index={1}>
+          <Typography variant='h2'>
+            Transaction History
+          </Typography>
+          <HistoryTable />
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <Typography variant='h2'>
+            Send Funds
+          </Typography>
+          <SendFundsForm />
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          <Typography variant='h2'>
+            Receive Funds
+          </Typography>
+          <ReceiveFundsForm />
+        </TabPanel>
+      </Paper>
+    </>
+  );
 };
 
 export default NavTabs;
