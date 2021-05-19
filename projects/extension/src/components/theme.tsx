cdf1f7985fd63357bdc3407ad544963c5561ec31
goldsteinsveta <sveta@parity.io>
2021-05-19 18:09:25 +0200

    indent theme file

--- projects/extension/src/components/theme.tsx
+++ projects/extension/src/components/theme.tsx
@@ -12,112 +12,112 @@ export const substrateGreen = {
 };
 
 const palette: PaletteOptions = {
-	type: 'light',
-	common: {
-		black: 'black',
-		white: 'white',
-	},
-	background: {
-		paper: 'white',
-		default: 'white',
-	},
-	primary: {
-		light: substrateGreen[100],
-		main: substrateGreen[400],
-		dark: substrateGreen[500],
-		contrastText: 'black',
-	},
-	secondary: {
-		light: substrateGreen[100],
-		main: substrateGreen[400],
-		dark: substrateGreen[500],
-		contrastText: 'black',
-	},
-	error: {
-		light: red[100],
-		main: '#FF3014',
-		dark: red[500],
-		contrastText: 'black',
-	},
-	text: {
-		primary: grey[900],
-		secondary: grey[500],
-		disabled: grey[300],
-		hint: grey[700],
-	},
-	action: {
-		active: substrateGreen[300],
-	},
-	divider: grey[300],
+  type: 'light',
+  common: {
+    black: 'black',
+    white: 'white',
+  },
+  background: {
+    paper: 'white',
+    default: 'white',
+  },
+  primary: {
+    light: substrateGreen[100],
+    main: substrateGreen[400],
+    dark: substrateGreen[500],
+    contrastText: 'black',
+  },
+  secondary: {
+    light: substrateGreen[100],
+    main: substrateGreen[400],
+    dark: substrateGreen[500],
+    contrastText: 'black',
+  },
+  error: {
+    light: red[100],
+    main: '#FF3014',
+    dark: red[500],
+    contrastText: 'black',
+  },
+  text: {
+    primary: grey[900],
+    secondary: grey[500],
+    disabled: grey[300],
+    hint: grey[700],
+  },
+  action: {
+    active: substrateGreen[300],
+  },
+  divider: grey[300],
 }
 
 const light: ThemeOptions = {
-	typography: {
-		allVariants: {
+  typography: {
+    allVariants: {
       color: palette.text && palette.text.primary,
     },
-		fontFamily: 'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, Segoe UI, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Web3-Regular"',
-		h1: {
-			fontWeight: 500,
-			fontSize: 30,
-			lineHeight: '120%',
-		},
-		h2: {
-			fontWeight: 400,
-			fontSize: 22,
-			lineHeight: '120%',
-			letterSpacing: 0.25,
-		},
-		h3: {
-			fontWeight: 500,
-			fontSize: 18,
-			lineHeight: '120%',
-		},
-		h4: {
-			fontWeight: 500,
-			fontSize: 15,
-			lineHeight: '120%',
-			color: grey[800],
-		},
-		body1: {
-			fontWeight: 400,
-			fontSize: 14,
-			lineHeight: '135%',
-			letterSpacing: 0.15,
-		},
-		body2: {
-			fontWeight: 400,
-			fontSize: 12,
-			lineHeight: '135%',
-			letterSpacing: 0.25,
-		},
-		button: {
-			fontWeight: 500,
-			fontSize: 14,
-			lineHeight: '120%',
-			letterSpacing: 0.2,
-			textTransform: 'none',
-		},
-		subtitle1: {
-			fontFamily: 'SFMono-Regular, Consolas , Liberation Mono, Menlo, monospace',
-			fontWeight: 400,
-			fontSize: 20,
-			lineHeight: '120%',
-		},
-		subtitle2: {
-			fontFamily: 'SFMono-Regular, Consolas , Liberation Mono, Menlo, monospace',
-			fontWeight: 400,
-			fontSize: 13,
-			lineHeight: '135%',
-			letterSpacing: 0.1,
-		},
-		overline: {
-			fontSize: 11,
-			lineHeight: '120%',
-			letterSpacing: 0.7,
-		},
-	},
-	palette: palette
+    fontFamily: 'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, Segoe UI, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Web3-Regular"',
+    h1: {
+      fontWeight: 500,
+      fontSize: 30,
+      lineHeight: '120%',
+    },
+    h2: {
+      fontWeight: 400,
+      fontSize: 22,
+      lineHeight: '120%',
+      letterSpacing: 0.25,
+    },
+    h3: {
+      fontWeight: 500,
+      fontSize: 18,
+      lineHeight: '120%',
+    },
+    h4: {
+      fontWeight: 500,
+      fontSize: 15,
+      lineHeight: '120%',
+      color: grey[800],
+    },
+    body1: {
+      fontWeight: 400,
+      fontSize: 14,
+      lineHeight: '135%',
+      letterSpacing: 0.15,
+    },
+    body2: {
+      fontWeight: 400,
+      fontSize: 12,
+      lineHeight: '135%',
+      letterSpacing: 0.25,
+    },
+    button: {
+      fontWeight: 500,
+      fontSize: 14,
+      lineHeight: '120%',
+      letterSpacing: 0.2,
+      textTransform: 'none',
+    },
+    subtitle1: {
+      fontFamily: 'SFMono-Regular, Consolas , Liberation Mono, Menlo, monospace',
+      fontWeight: 400,
+      fontSize: 20,
+      lineHeight: '120%',
+    },
+    subtitle2: {
+      fontFamily: 'SFMono-Regular, Consolas , Liberation Mono, Menlo, monospace',
+      fontWeight: 400,
+      fontSize: 13,
+      lineHeight: '135%',
+      letterSpacing: 0.1,
+    },
+    overline: {
+      fontSize: 11,
+      lineHeight: '120%',
+      letterSpacing: 0.7,
+    },
+  },
+  palette: palette
 };
 
 export default light;
