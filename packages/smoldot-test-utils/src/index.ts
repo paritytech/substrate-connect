066195a34d4499a2c63d9d13f92a56c635be1769
Raoul Millais <raoul@raoulmillais.com>
2021-06-04 17:49:40 +0100

    Rearrange helper functions and add tsdocs

--- packages/smoldot-test-utils/src/index.ts
+++ packages/smoldot-test-utils/src/index.ts
@@ -8,29 +8,95 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { SmoldotClient, SmoldotOptions } from 'smoldot';
 
-type RpcResponder = (request: string) => string;
+/**
+ * Response builders - functions that simplify making RPC responses
+ */
 
+/**
+ * systemHealthReponse - creates a valid RPC response that looks like a response
+ * to a "system_health" RPC request using the supplied arguments
+ *
+ * @returns a JSON string
+ */
 const systemHealthReponse = (id: number, peerCount: number) => {
   return `{"jsonrpc":"2.0","id":${id} ,"result":{"isSyncing":true,"peers":${peerCount},"shouldHavePeers":true}}`;
 }
 
-// Always responds to a health request with a response that has peers
+/**
+ * devChainHealthResponse - creates a valid RPC response that looks like a response
+ * to a "system_health" RPC request that came from a "dev chain"
+ *
+ * @returns a JSON string
+ *
+ * @remarks
+ *
+ * Dev chains never have peers
+ */
+const devChainHealthResponse = (id: number) => {
+  return `{"jsonrpc":"2.0","id":${id} ,"result":{"isSyncing":true,"peers":0,"shouldHavePeers":false}}`;
+}
+
+/**
+ * RpcResponder - an RpcResponder is a type representing a function that takes
+ * a JSON string representing an RPC request and uses it to create a JSON RPC
+ * response.
+ */
+type RpcResponder = (request: string) => string;
+
+/**
+ * healthyResponder - takes a JSON request that is expected to look like a valid
+ * "system_health" RPC request and returns a valid matching healthy response. It
+ * takes the ID from the request and ensures the response has the same ID.
+ *
+ * @param requestJSON
+ * @returns a JSON string
+ */
 const healthyResponder = (requestJSON: string) => {
   const id = JSON.parse(requestJSON).id;
   return systemHealthReponse(id, 1);
 }
 
-// Always responds to a health request with a response that has no peers
+/**
+ * unhealthyResponder - takes a JSON request that is expected to look like a valid
+ * "system_health" RPC request and returns a valid matching unhealthy response. It
+ * takes the ID from the request and ensures the response has the same ID. The
+ * response will say there are no peers.
+ *
+ * @param requestJSON
+ * @returns a JSON string
+ */
 const unhealthyResponder = (requestJSON: string) => {
   const id = JSON.parse(requestJSON).id;
   return systemHealthReponse(id, 0);
 }
 
-// Always responds with an error reponse
+/**
+ * devChainHealthResponder - takes a JSON request that is expected to look like
+ * a valid "system_health" RPC request and returns a valid matching healthy
+ * response for a "dev chain". It takes the ID from the request and ensures the
+ * response has the same ID.
+ *
+ * @param requestJSON
+ * @returns a JSON string
+ */
+export const devChainHealthResponder = (requestJSON: string) => {
+  const id = JSON.parse(requestJSON).id;
+  return devChainHealthResponse(id);
+}
+
+/**
+ * unhealthyResponder - takes a JSON request that is expected to look like any
+ * valid RPC request and and always generates an RPC error response.
+ *
+ * @param requestJSON
+ * @returns a JSON string
+ */
 export const erroringResponder = (requestJSON: string) => {
   const id = JSON.parse(requestJSON).id;
   return `{ "id": ${id}, "jsonrpc": "2.0", "error": {"code": 666, "message": "boom!" } }`;
 }
+
+// ---
 
 // Orchestrates a sequence of has peers / has no peers responses
 export const customHealthResponder = (hasPeers: boolean[]) => {
@@ -43,16 +109,6 @@ export const customHealthResponder = (hasPeers: boolean[]) => {
   };
 }
 
-// dev chains never have peers
-const devChainHealthResponse = (id: number) => {
-  return `{"jsonrpc":"2.0","id":${id} ,"result":{"isSyncing":true,"peers":0,"shouldHavePeers":false}}`;
-}
-
-export const devChainHealthResponder = (requestJSON: string) => {
-  const id = JSON.parse(requestJSON).id;
-  return devChainHealthResponse(id);
-
-}
 
 // Mimics the behaviour of the WASM light client by deferring a call to 
 // `jsonRpcCallback` after it is called which returns the response
