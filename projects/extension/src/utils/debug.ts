b20e3fda1c628429678304706e1d56b7b530f230
wirednkod <wirednkod@gmail.com>
2021-06-08 15:26:34 +0300

    remove empty spaces;  remove tests irrelevant to the PR in order to add them in different PR

--- projects/extension/src/utils/debug.ts
+++ projects/extension/src/utils/debug.ts
@@ -1,16 +1,16 @@
-/*	
- * Prints a debug message to the console when running in local development	
- * mode.	
- *	
- * @param message - a descriptive message to print	
- * @param ctx - an optional object to log out to the console	
- */	
- export function debug(message: string, ctx?: unknown): void {	
-  if (process.env.NODE_ENV === 'development') {	
-    if (ctx) {	
-      console.debug(message, ctx);	
-    } else {	
-      console.debug(message);	
-    }	
-  }	
-}	
+/*
+ * Prints a debug message to the console when running in local development
+ * mode.
+ *
+ * @param message - a descriptive message to print
+ * @param ctx - an optional object to log out to the console
+ */
+export function debug(message: string, ctx?: unknown): void {
+  if (process.env.NODE_ENV === 'development') {
+    if (ctx) {
+      console.debug(message, ctx);
+    } else {
+      console.debug(message);
+    }
+  }
+}
