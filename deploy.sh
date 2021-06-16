d470d6fa345782d81c7e50a8c6fd50cf2e40fe84
wirednkod <wirednkod@gmail.com>
2021-06-17 00:52:33 +0300

    deactivate extension .zip of deploy script

--- deploy.sh
+++ deploy.sh
@@ -34,8 +34,8 @@ deployGhPages() {
   cp -r ./projects/smoldot-browser-demo/dist/* ./$directory/smoldot-browser-demo/.
   echo "Place Multiple network demo's files."
   cp -r ./projects/multiple-network-demo/dist/* ./$directory/multiple-network-demo/.
-  echo "Place Substrate-connect extension's zip."
-  cp ./projects/extension/dist/substrate-connect.zip ./$directory/extension/substrate-connect.zip
+  # echo "Place Substrate-connect extension's zip."
+  # cp ./projects/extension/dist/substrate-connect.zip ./$directory/extension/substrate-connect.zip
   echo "Place landing page's files."
   cp -r ./projects/landing-page/dist/* ./$directory/.
  }
