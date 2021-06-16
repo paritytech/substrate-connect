1fb0302ab68e6c632dc68ade4dd411bc5c662355
Raoul Millais <raoul@raoulmillais.com>
2021-06-16 16:22:57 +0100

    Tidy specific rules from burnr / landing-page
    - Reinstate no-misused-promises everywhere (there were no errors)
    - Promote config for restrict-plus-operands to the root

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,33 @@
 module.exports = {
-    root: true,
-    rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        { "argsIgnorePattern": "^_" }
-      ],
-      "@typescript-eslint/restrict-template-expressions": "off",
-      "tsdoc/syntax": "error"
-    },
-    parser: "@typescript-eslint/parser",
-    settings: { react: { version: "detect" } },
-    plugins: [
-      "@typescript-eslint",
-      "eslint-plugin-tsdoc"
+  root: true,
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { "argsIgnorePattern": "^_" }
     ],
-    parserOptions: {
-      project: "./tsconfig.json",
-      createDefaultProgram: true
-    },
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    "@typescript-eslint/restrict-plus-operands": [
+      "error",
+      { "checkCompoundAssignments": false }
     ],
-    env: {
-      browser: true
-    }
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "tsdoc/syntax": "error"
+  },
+  parser: "@typescript-eslint/parser",
+  settings: { react: { version: "detect" } },
+  plugins: [
+    "@typescript-eslint",
+    "eslint-plugin-tsdoc"
+  ],
+  parserOptions: {
+    project: "./tsconfig.json",
+    createDefaultProgram: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+  ],
+  env: {
+    browser: true
+  }
 };
--- projects/burnr/.eslintrc.js
+++ projects/burnr/.eslintrc.js
@@ -2,19 +2,7 @@ module.exports = {
   rules: {
     "react/prop-types": 0,
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      {
-        "checksConditionals": false
-      }
-    ],
-    "@typescript-eslint/restrict-plus-operands": [
-      "error",
-      {
-        "checkCompoundAssignments": false
-      }
-    ]
+    "react-hooks/exhaustive-deps": "warn"
   },
   plugins: [
     "react-hooks"
