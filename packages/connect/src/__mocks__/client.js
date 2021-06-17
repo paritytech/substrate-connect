7a24abf289f9d7dc24150f3e5cd122cd3dbc5587
Raoul Millais <raoul@raoulmillais.com>
2021-06-17 10:14:11 +0100

    Remove unneeded jsdom reset in connect mocks

--- packages/connect/src/__mocks__/client.js
+++ packages/connect/src/__mocks__/client.js
@@ -2,12 +2,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { JSDOM } from 'jsdom';
-const dom = new JSDOM();
-
-window.document = dom.window.document;
-window.window = dom.window;
-
 const localStorageMock = (() => {
     let store = {};
     return {
