816434e8f9f6244923c1d1ad91848d50df14b938
wirednkod <wirednkod@gmail.com>
2021-06-18 11:12:28 +0300

    Alter dropdown to label showing the one connected network

--- projects/burnr/src/components/index.tsx
+++ projects/burnr/src/components/index.tsx
@@ -12,6 +12,7 @@ export { default as LogoSubstrate } from './LogoSubstrate';
 export { default as NavFooter } from './NavFooter';
 export { default as NavTabs } from './NavTabs';
 export { default as NodeSelector } from './NodeSelector';
+export { default as NodeConnected } from './NodeConnected';
 export { default as PopoverInfo } from './PopoverInfo';
 export { default as PopoverExtrinsic } from './PopoverExtrinsic';
 export { default as SendFundsForm } from './SendFundsForm';
