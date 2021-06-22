70cbc5f1a232523e7a5c0161ba6e5c15c0060e7e
Raoul Millais <raoul@raoulmillais.com>
2021-06-22 17:29:45 +0100

    Fix test config to only look in dist
    - Also extensionsToTreatAsEsm was wrong wince we have already transpiled

--- packages/connect/jest.config.ts
+++ packages/connect/jest.config.ts
@@ -1,10 +1,10 @@
 import type {Config} from '@jest/types';
 
 const config: Config.InitialOptions = {
+  roots: ['<rootDir>/dist'],
   verbose: true,
   transformIgnorePatterns:["./src/__mocks__/client.js"],
   testURL: "http://localhost/",
-  transform: {},
-  extensionsToTreatAsEsm: [".ts", ".tsx"]
+  transform: {}
 };
 export default config;
