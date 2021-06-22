a2e1fbb8abc463d9888c227a566971e7d945cdd8
Raoul Millais <raoul@raoulmillais.com>
2021-06-22 17:49:55 +0100

    Only show failing tests when running tests

--- packages/connect/jest.config.ts
+++ packages/connect/jest.config.ts
@@ -1,6 +1,8 @@
 import type {Config} from '@jest/types';
 
 const config: Config.InitialOptions = {
+  coverageReporters: ["text-summary"],
+  reporters: ["jest-silent-reporter"],
   roots: ['<rootDir>/dist'],
   verbose: true,
   transformIgnorePatterns:["./src/__mocks__/client.js"],
