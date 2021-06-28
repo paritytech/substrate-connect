f95398e0979073876775ee458be8aab7d751f44c
wirednkod <wirednkod@gmail.com>
2021-06-28 19:53:53 +0300

    Fix the reconnection issue that exists when browser was restarted; Fixes #363

--- projects/extension/src/background/index.ts
+++ projects/extension/src/background/index.ts
@@ -4,6 +4,7 @@ import { ConnectionManager } from './ConnectionManager';
 import westend from '../../public/assets/westend.json';
 import kusama from '../../public/assets/kusama.json';
 import polkadot from '../../public/assets/polkadot.json';
+import { logger } from '@polkadot/util';
 export interface Background extends Window {
   manager: ConnectionManager
 }
@@ -12,6 +13,7 @@ declare let window: Background;
 
 const manager = window.manager = new ConnectionManager();
 
+const l = logger('Extension');
 export interface RequestRpcSend {
   method: string;
   params: unknown[];
@@ -26,12 +28,18 @@ const createSmoldot = async (name: string, spec: unknown) => {
   }
 }
 
-chrome.runtime.onInstalled.addListener(() => {
-  void createSmoldot('polkadot', polkadot);
-  void createSmoldot('kusama', kusama);
-  void createSmoldot('westend', westend);
-});
+const init = () => {
+  createSmoldot('polkadot', polkadot).catch(err => l.error('Error', err));
+  createSmoldot('kusama', kusama).catch(err => l.error('Error', err));
+  createSmoldot('westend', westend).catch(err => l.error('Error', err));
+}
+
+chrome.runtime.onStartup.addListener(() => { init(); });
 
 chrome.runtime.onConnect.addListener((port) => {
   manager.addApp(port);
 });
+
+// TODO (nik): once extension is on chrome/ff stores we need to take advantage 
+// of the onBrowserUpdateAvailable and onUpdateAvailable lifecycle event
+// NOTE: onSuspend could be used to cleanup things but async actions are not guaranteed to complete :(
