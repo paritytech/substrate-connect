ad986a31d62be690b831b1ac2a9a914d68815ff8
Nikos Kontakis <wirednkod@gmail.com>
2021-07-05 15:45:04 +0300

    add footer links (#386)

--- projects/burnr/src/components/NavFooter.tsx
+++ projects/burnr/src/components/NavFooter.tsx
@@ -7,22 +7,22 @@ const AppFooter: React.FunctionComponent = () => (
     <Typography variant='body2' component='div'>
       <Grid container spacing={1}>
         <Grid item>
-          <Link href='#' color='textPrimary'>
+          <Link href='https://www.parity.io/' underline="hover" color='textPrimary'>
           © {new Date().getFullYear()} Parity Technologies
           </Link>
         </Grid>
         <Grid item>
-          <Link href='#' color='textPrimary'>
+          <Link href='https://substrate.dev/terms' underline="hover" target="_blank" rel="noreferrer" color='textPrimary'>
         Terms & conditions
           </Link>
         </Grid>
         <Grid item>
-          <Link href='#' color='textPrimary'>
+          <Link href='https://www.parity.io/privacy/' underline="hover" target="_blank" rel="noreferrer" color='textPrimary'>
         Privacy policy
           </Link>
         </Grid>
         <Grid item>
-          <Link href='#' color='textPrimary'>
+          <Link href='https://github.com/paritytech/substrate-connect/issues' underline="hover" target="_blank" rel="noreferrer" color='textPrimary'>
         Report an issue
           </Link>
         </Grid>
