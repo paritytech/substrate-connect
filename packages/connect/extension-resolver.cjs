e52b0700d30b80fb5bbd6e41d7370d08933b52d1
wirednkod <wirednkod@gmail.com>
2021-07-09 16:07:16 +0300

    fix PR comments

--- packages/connect/extension-resolver.cjs
+++ packages/connect/extension-resolver.cjs
@@ -1,17 +1,8 @@
 let defaultResolver;
 
 function requireDefaultResolver() {
-  if (!defaultResolver) {
-    try {
-      defaultResolver = import (`jest-resolve/build/defaultResolver`);
-    } catch (error) {
-      defaultResolver = import (`jest-resolve/build/default_resolver`);
-    }
-  }
-
-  return defaultResolver;
+  return import (`jest-resolve/build/defaultResolver`);
 }
-
 
 module.exports = (request, options) => {
   const {basedir, defaultResolver, extensions} = options;
@@ -22,8 +13,21 @@ module.exports = (request, options) => {
 
   try {
     return defaultResolver(request, options);
-  } catch (e) {
-    console.log(`${e} - Temporary replace extension '.js' to '.ts' for tests to run.\n`)
-    return defaultResolver(request.replace(/\.js$/, '.ts'), options);
+  } catch (err) {
+    if (
+      // We should not try to fix issues of other packages
+      !err.message.includes('node_modules') &&
+      request.endsWith('.js') && // make sure that the file ends with .js
+      err.code === 'MODULE_NOT_FOUND' // make sure error code is specific
+    ) {
+      try {
+        console.log(`Temporary replace extension '.js' to '.ts' for tests to run.\n`)
+        return defaultResolver(request.replace(/\.js$/, '.ts'), options);  
+      } catch (e) {
+        console.error(`Errored resolving a module after replacing js extension with ts.${err.message}\n`);
+        console.error(`${e}`);
+      }
+    }
+    throw e;
   }
 }
