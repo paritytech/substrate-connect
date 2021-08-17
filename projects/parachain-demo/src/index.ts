0ad269fd5e21869bf3596fe31934579fd98c04e7
wirednkod <wirednkod@gmail.com>
2021-08-17 13:10:23 +0300

    add parachain demo

---
+++ projects/parachain-demo/src/index.ts
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+// hack to make poladot-js work without bringing in webpack and babel
+import "regenerator-runtime/runtime"
+import { Detector }  from '@substrate/connect';
+import westmint from './assets/westend-westmint.json';
+import UI, { emojis } from './view';
+
+window.onload = () => {
+  const loadTime = performance.now();
+  const ui = new UI({ containerId: 'messages' }, { loadTime });
+  ui.showSyncing();
+
+  (async () => {
+    try {
+      const detect = new Detector('Parachain Demo');
+      const api = await detect.connect('westend', {name: 'Westmint', spec: JSON.stringify(westmint)});
+      console.log('api', api);
+      const header = await api.rpc.chain.getHeader()
+      const chainName = await api.rpc.system.chain()
+
+      // Show chain constants - from chain spec
+      ui.log(`${emojis.seedling} Light client ready`, true);
+      ui.log(`${emojis.info} Connected to ${chainName}: syncing will start at block #${header.number}`);
+      // ui.log(`${emojis.chequeredFlag} Genesis hash is ${api.genesisHash.toHex()}`);
+      // ui.log(`${emojis.clock} Epoch duration is ${api.consts.babe.epochDuration.toNumber()} blocks`);
+      // ui.log(`${emojis.banknote} ExistentialDeposit is ${api.consts.balances.existentialDeposit.toHuman()}`);
+
+      // Show how many peers we are syncing with
+      const health = await api.rpc.system.health();
+      const peers = health.peers.toNumber() === 1 ? '1 peer' : `${health.peers} peers`;
+      ui.log(`${emojis.stethoscope} Chain is syncing with ${peers}`);
+
+      const waitForChainToSync = () => {
+        let resolved = false;
+        return new Promise<void>((resolve, reject) => {
+          setInterval(() => {
+            api.rpc.system.health().then(health => {
+              if (health.isSyncing.eq(false)) {
+                ui.showSynced();
+                if (!resolved) {
+                  resolved = true;
+                  resolve();
+                }
+              } else {
+                ui.showSyncing();
+              }
+            }).catch(error => {
+              ui.error(error);
+              if (!resolved) {
+                resolved = true;
+                reject();
+              }
+            });
+          }, 2000);
+        });
+      }
+
+      await waitForChainToSync();
+      ui.log(`${emojis.newspaper} Subscribing to new block headers`);
+      await api.rpc.chain.subscribeNewHeads((lastHeader) => {
+        ui.log(`${emojis.brick} New block #${lastHeader.number} has hash ${lastHeader.hash}`);
+      });
+    } catch (error) {
+        ui.error(error);
+    }
+
+  })();
+};
