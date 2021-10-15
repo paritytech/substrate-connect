3f1d9d99bee7fbdedf740d7540fa56ca212e22df
wirednkod <wirednkod@gmail.com>
2021-10-15 22:04:11 +0300

    fix linter issues

--- projects/multiple-network-demo/webpack.config.js
+++ projects/multiple-network-demo/webpack.config.js
@@ -1,57 +1,56 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-import { createRequire } from 'module';
-import { DefinePlugin, ProvidePlugin, HotModuleReplacementPlugin } from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-export const mode = "development";
-export const entry = "./src/index.ts";
-export const devtool = "inline-source-map";
-export const devServer = {
-  port: 3000,
-  open: true,
-  hot: true,
-};
-export const module = {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env"],
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
       },
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "react/jsx-runtime": require.resolve("react/jsx-runtime"),
     },
-    { test: /\.tsx?$/, loader: "ts-loader" },
-    {
-      test: /\.css$/i,
-      use: ["style-loader", "css-loader"],
+    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
     },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.WS_URL": JSON.stringify(undefined),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
 };
-
-const require = createRequire(import.meta.url);
-
-export const resolve = {
-  alias: {
-    "react/jsx-runtime": require.resolve("react/jsx-runtime"),
-  },
-  extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
-  fallback: {
-    crypto: require.resolve("crypto-browserify"),
-    stream: require.resolve("stream-browserify"),
-  },
-};
-export const plugins = [
-  new DefinePlugin({
-    "process.env.WS_URL": JSON.stringify(undefined),
-  }),
-  new HtmlWebpackPlugin({
-    template: "./index.html",
-  }),
-  new ProvidePlugin({
-    Buffer: ["buffer", "Buffer"],
-  }),
-  new HotModuleReplacementPlugin(),
-];
