f25f973c2d439c389d3c90ab6606b623d4bacf1b
Nikos Kontakis <wirednkod@gmail.com>
2021-10-19 14:01:17 +0300

    Convert multiple network demo to webpack (#518)
    * Convert multiple network demo to webpack
    
    * fix linter issues
    
    * fix linter issues
    
    * Update webpack.config.js
    
    Remove non-needed webpack plugin

---
+++ projects/multiple-network-demo/webpack.config.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "react/jsx-runtime": require.resolve("react/jsx-runtime"),
+    },
+    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
