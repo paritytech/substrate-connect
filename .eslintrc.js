52634e65c8f3381613d79fccd484fc1fc0fc7ee6
Josep M Sobrepere <jm.sobrepere@gmail.com>
2021-10-20 11:43:52 +0200

    chore: making prettier run transparently

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
   ],
   env: {
     browser: true,
