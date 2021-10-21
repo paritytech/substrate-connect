0eff20d440ddd71da5163eb9e718b32946060cbc
wirednkod <wirednkod@gmail.com>
2021-10-21 12:53:04 +0300

    Remove eslint disablings

--- bin/checkSpecs.js
+++ bin/checkSpecs.js
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 const fs = require("fs")
 const path = require("path")
 const networks = [
--- bin/downloadSpecs.js
+++ bin/downloadSpecs.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-undef */
-/* eslint-disable @typescript-eslint/no-var-requires */
 const http = require("https")
 const fs = require("fs")
 
