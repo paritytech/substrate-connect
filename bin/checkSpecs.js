65b57bf2fd3d50626f689b92380294824a2673a6
wirednkod <wirednkod@gmail.com>
2021-10-21 12:44:36 +0300

    Fix spec checks based on Id and download scripts

--- bin/checkSpecs.js
+++ bin/checkSpecs.js
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 const fs = require("fs")
 const path = require("path")
 const networks = [
@@ -14,6 +17,7 @@ const paths = {
   extension: "public/assets",
 }
 
+// Identify where the script is called from (can be either "extension" or "connect")
 const workspace = path.resolve().split("/").pop()
 const workspacePath = `${path.resolve()}/${paths[workspace]}`
 const pathExist = fs.existsSync(workspacePath)
@@ -22,11 +26,26 @@ const pathExist = fs.existsSync(workspacePath)
 if (!pathExist) fs.mkdirSync(workspacePath)
 
 networks.forEach((network) => {
-  const file = `${workspacePath}/${network}.json`
-  if (!fs.existsSync(file)) {
-    console.log(`File ${file} does not exist. Copying...`)
-    fs.copyFile(`${chainDir}/${network}.json`, file, (err) => {
+  const wsPathFile = `${workspacePath}/${network}.json`
+  const chainDirFile = `${chainDir}/${network}.json`
+  if (!fs.existsSync(wsPathFile)) {
+    console.log(`File ${wsPathFile} does not exist. Copying...`)
+    fs.copyFile(`${chainDir}/${network}.json`, wsPathFile, (err) => {
       if (err) throw err
+    })
+  } else {
+    // file exists - we should check if IDs are same
+    fs.readFile(wsPathFile, function (err, data) {
+      if (err) console.log(err)
+      const dirFile = JSON.parse(data).id
+      fs.readFile(chainDirFile, function (err, data) {
+        if (err) console.log(err)
+        if (JSON.parse(data).id !== dirFile) {
+          fs.copyFile(chainDirFile, wsPathFile, (err) => {
+            if (err) throw err
+          })
+        }
+      })
     })
   }
 })
