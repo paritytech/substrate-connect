5357bb9eae10e4e63c498fcc0e39171a8475e68b
goldsteinsveta <sveta@parity.io>
2021-12-06 16:09:53 +0100

    rm unused (theme)

--- projects/extension/src/components/Logo.tsx
+++ projects/extension/src/components/Logo.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { makeStyles, Typography } from "@material-ui/core"
 import { substrateGreen } from "./theme"
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   root: {
     fontWeight: 600,
     lineHeight: 1,
@@ -13,7 +13,7 @@ const useStyles = makeStyles((theme) => ({
       fontWeight: 400,
     },
   },
-}))
+})
 
 const Logo: React.FunctionComponent = () => {
   const classes = useStyles()
