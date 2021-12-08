cebce3d91d092d35bc5da5957d9e65f2973a8a0e
Pierre Krieger <pierre.krieger1708@gmail.com>
2021-12-08 17:38:30 +0100

    Fix linting

--- packages/smoldot-test-utils/src/index.ts
+++ packages/smoldot-test-utils/src/index.ts
@@ -260,6 +260,7 @@ export const createAddChain = (opts: {
 }): Promise<Chain> => {
   return Promise.resolve({
     sendJsonRpc: opts.jsonRpcCallback,
+    // eslint-disable-next-line @typescript-eslint/require-await
     databaseContent: async () => "",
     remove: doNothing,
   })
