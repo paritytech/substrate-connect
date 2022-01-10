bfc694bfda799f39a2180c0fab1beeb979dd446d
Josep M Sobrepere <jm.sobrepere@gmail.com>
2022-01-10 15:30:17 +0100

    Add `chainId` into `ToApplication` interface

--- packages/connect/src/ExtensionProvider/ExtensionProvider.test.ts
+++ packages/connect/src/ExtensionProvider/ExtensionProvider.test.ts
@@ -119,6 +119,7 @@ test("disconnects and emits an error when it receives an error message", async (
   await waitForMessageToBePosted()
   sendMessage({
     origin: "content-script",
+    chainId: ep.chainId,
     type: "error",
     payload: "disconnected",
   })
@@ -133,6 +134,7 @@ test("emits error when it receives an error message", async () => {
   await waitForMessageToBePosted()
   const errorMessage: ToApplication = {
     origin: "content-script",
+    chainId: ep.chainId,
     type: "error",
     payload: "Boom!",
   }
@@ -144,4 +146,62 @@ test("emits error when it receives an error message", async () => {
   expect(errorHandler).toHaveBeenCalled()
   const error = errorHandler.mock.calls[0][0] as Error
   expect(error.message).toEqual(errorMessage.payload)
+})
+
+test("it routes incoming messages to the correct Provider", async () => {
+  const ep1 = new ExtensionProvider("ExtensionProvider1", westendSpec)
+  await ep1.connect()
+
+  const ep2 = new ExtensionProvider("ExtensionProvider2", westendSpec)
+  await ep2.connect()
+  await waitForMessageToBePosted()
+
+  let extensionProvider1Response: string | undefined = undefined
+  let extensionProvider2Response: string | undefined = undefined
+
+  ep1
+    .send("requestSomeData", [])
+    .then((response: string) => {
+      extensionProvider1Response = response
+    })
+    .catch(() => {
+      extensionProvider1Response = "Error"
+    })
+  await waitForMessageToBePosted()
+
+  ep2
+    .send("requestOtherData", [])
+    .then((response: string) => {
+      extensionProvider2Response = response
+    })
+    .catch(() => {
+      extensionProvider2Response = "Error"
+    })
+  await waitForMessageToBePosted()
+
+  const latestRequest = handler.mock.calls[
+    handler.mock.calls.length - 1
+  ][0] as MessageEvent<{ payload: string }>
+
+  const latestRequestRpcId = (
+    JSON.parse(latestRequest?.data.payload ?? "{}") as {
+      id: number
+    }
+  ).id
+
+  sendMessage({
+    origin: "content-script",
+    chainId: ep2.chainId,
+    type: "rpc",
+    payload: JSON.stringify({
+      id: latestRequestRpcId,
+      jsonrpc: "2.0",
+      result: "hi ExtensionProvider2!",
+    }),
+  })
+
+  await waitForMessageToBePosted()
+
+  expect(extensionProvider2Response).toBe("hi ExtensionProvider2!")
+  expect(extensionProvider1Response).toBe(undefined)
 })
