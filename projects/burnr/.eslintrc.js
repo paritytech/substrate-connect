af5542d3365ebe2794d1231959055a7d7ec7f78b
wirednkod <wirednkod@gmail.com>
2022-01-12 18:10:45 +0200

    update lint and webpack configs

--- projects/burnr/.eslintrc.js
+++ projects/burnr/.eslintrc.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   rules: {
     "react/prop-types": 0,
@@ -5,5 +6,5 @@ module.exports = {
     "react-hooks/exhaustive-deps": "warn",
   },
   plugins: ["react-hooks"],
-  extends: ["plugin:react/recommended"],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
 }
