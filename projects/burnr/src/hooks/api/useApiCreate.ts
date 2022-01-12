46163c922c7f7a2a4bf1e465b1915e4714f7a216
wirednkod <wirednkod@gmail.com>
2022-01-12 18:30:09 +0200

    fix lint error

--- projects/burnr/src/hooks/api/useApiCreate.ts
+++ projects/burnr/src/hooks/api/useApiCreate.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { useEffect, useState } from "react"
 import { ApiPromise } from "@polkadot/api"
 import { logger } from "@polkadot/util"
