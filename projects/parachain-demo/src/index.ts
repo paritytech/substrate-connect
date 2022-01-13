2bee41352451327d9b6bb1cc29a49fbced0ce593
wirednkod <wirednkod@gmail.com>
2022-01-13 17:27:58 +0200

    minor revert of eslint disables

--- projects/parachain-demo/src/index.ts
+++ projects/parachain-demo/src/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 import { ScProvider, SupportedChains } from "@substrate/connect"
