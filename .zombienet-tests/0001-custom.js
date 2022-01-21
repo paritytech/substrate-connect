ec0acf8972db1690714752be6ac401dc5e2339f2
Javier Viola <pepoviola@gmail.com>
2022-01-21 12:00:07 -0300

    wip, debug

--- .zombienet-tests/0001-custom.js
+++ .zombienet-tests/0001-custom.js
@@ -12,12 +12,13 @@ async function run(nodeName, networkInfo) {
     const {userDefinedTypes} = networkInfo.nodesByName[nodeName];
     const customChainSpec = require(networkInfo.chainSpecPath);
     console.log("bootnodes");
-    console.log(customChainSpec.bootNodes);
-    customChainSpec.bootNodes = customChainSpec.bootNodes.map(addr => addr.replace("localhost", "127.0.0.1"));
+    customChainSpec.bootNodes = customChainSpec.bootNodes.map(addr => {
+        return addr.replace("localhost", "127.0.0.1").replace("p2p","ws")
+    });
     console.log(customChainSpec.bootNodes);
     const api = await connect(JSON.stringify(customChainSpec), userDefinedTypes);
     const validator = await api.query.session.validators();
-    console.log("validators",validators);
+    console.log("validators",validator);
     return validator.length;
 }
 
