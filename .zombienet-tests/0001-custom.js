8d1f79e639369be88f711a399ee20d9db3642b19
Javier Viola <pepoviola@gmail.com>
2022-01-22 18:54:44 -0300

    fix formating

--- .zombienet-tests/0001-custom.js
+++ .zombienet-tests/0001-custom.js
@@ -1,24 +1,24 @@
-const polkadotApi = require("@polkadot/api");
+const polkadotApi = require("@polkadot/api")
 
 async function connect(customChainSpec, types) {
-    const substrateConnect = await import("@substrate/connect");
-    const provider = new substrateConnect.ScProvider(customChainSpec)
-    const api = new polkadotApi.ApiPromise({ provider, types });
-    await api.isReady;
-    return api;
+  const substrateConnect = await import("@substrate/connect")
+  const provider = new substrateConnect.ScProvider(customChainSpec)
+  const api = new polkadotApi.ApiPromise({ provider, types })
+  await api.isReady
+  return api
 }
 
 async function run(nodeName, networkInfo) {
-    const {userDefinedTypes} = networkInfo.nodesByName[nodeName];
-    const customChainSpec = require(networkInfo.chainSpecPath);
+  const { userDefinedTypes } = networkInfo.nodesByName[nodeName]
+  const customChainSpec = require(networkInfo.chainSpecPath)
 
-    // TODO: forkId generate an error in smoldot-light
-    delete customChainSpec.forkId;
+  // TODO: forkId generate an error in smoldot-light
+  delete customChainSpec.forkId
 
-    const api = await connect(JSON.stringify(customChainSpec), userDefinedTypes);
-    const validator = await api.query.session.validators();
-    console.log("validators",validator);
-    return validator.length;
+  const api = await connect(JSON.stringify(customChainSpec), userDefinedTypes)
+  const validator = await api.query.session.validators()
+  console.log("validators", validator)
+  return validator.length
 }
 
 module.exports = { run }
