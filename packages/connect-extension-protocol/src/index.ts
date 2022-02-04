bd40dd3af4e1f4ce6cb7eb753b74f3d4b5055f4f
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-02-04 11:49:11 +0100

    Document the web page <-> extension protocol (#711)
    * Document the web page <-> extension protocol
    
    * Add note about well-known chains
    
    * Typo
    
    * Typos
    
    * More typo

--- packages/connect-extension-protocol/src/index.ts
+++ packages/connect-extension-protocol/src/index.ts
@@ -1,23 +1,84 @@
 /**
  * @packageDocumentation
  *
- * In order to understand the protocol you should realise there are actually
- * 2 hops that happen in communication because of the architecture of browser
- * extensions.  The app has to `window.postMessage` messages to the content
- * script that gets injected by the extension. It is the content script that
- * has access to the extension APIs to be able to post messages to the
- * extension background.
+ * This module contains the types and explanations of the communication
+ * protocol between the JavaScript code embedded in a web page and the
+ * substrate-connect extension.
  *
- * You can think of the protocol types like layers of an onion. The innermost
- * layer is the original JSON RPC request/responses. Then we wrap extra layers
- * (types) for the other 2 hops which then get peeled off at each hop.
- * The {@link ToExtension} and {@link ToApplication} represents
- * communication between the PolkadotJS provider in the app and the extension
- * (content and background scripts).
+ * # Overview
  *
- * The {@link ExtensionProvider} is the class in the app.
- * The {@link ExtensionMessageRouter} is the class in the content script.
- * The {@link ConnectionManager} is the class in the extension background.
+ * TODO: explain how the web page can detect whether the extension is installed
+ *
+ * If a web page wants to use the features of the substrate-connect extension,
+ * it can do so by sending messages on its `window` by using
+ * `Window.postMessage`. These messages must conform to the {@link ToExtension}
+ * interface defined below.
+ *
+ * The substrate-connect extension (more precisely, its content-script) listens
+ * for "message" events (using `window.addEventListener("message", ...)`) and
+ * replies by sending back messages using `Window.postMessage` as well. The
+ * messages sent by the extension conform to the {@link ToApplication}
+ * interface defined below.
+ *
+ * # Detailed usage
+ *
+ * In order to ask the substrate-connect extension to connect to a certain
+ * chain, the web page must:
+ *
+ * - Randomly generate the so-called `chainId`, a string that will be used
+ * to identify this specific chain connection during its lifetime. At least
+ * 48 bits of entropy are recommended in order to avoid accidentally
+ * generating the same string multiple times.
+ * - Send a {@link ToExtensionAddChain} message (using `Window.postMessage`,
+ * as explained in the previous section) containing this `chainid` and the
+ * specification of the chain to connect to.
+ *
+ * Instead of a {@link ToExtensionAddChain} message, the web page can
+ * alternatively send a {@link ToExtensionAddWellKnownChain} message and pass
+ * a chain name recognized by the extension such as "polkadot" or "kusama", in
+ * which case the extension will use the chain specification stored internally.
+ * Doing so provides multiple advantages such as less bandwidth usage (as the
+ * web page doesn't have to download the chain specification), and a faster
+ * initialization as the extension is most likely already connected to that
+ * chain.
+ *
+ * After a {@link ToExtensionAddChain} or a
+ * {@link ToExtensionAddWellKnownChain} message has been sent, the extension
+ * starts connecting to the chain, and later replies by sending back a
+ * {@link ToApplicationChainReady} message in case of success, or a
+ * {@link ToApplicationError} message in case of failure. This reply might
+ * only be sent back after a few seconds or more, and the web page is
+ * encouraged to display some kind of loading screen in the meanwhile.
+ *
+ * Note that the extension reserves the rights to stop supporting a chain that
+ * used to be recognized by {@link ToExtensionAddWellKnownChain}. If the web
+ * page has sent a {@link ToExtensionAddWellKnownChain} and receives back a
+ * {@link ToApplicationError}, it should automatically fall back to
+ * downloading the chain specification and sending a
+ * {@link ToExtensionAddChain} instead.
+ *
+ * After a chain has been successfully initialized (i.e. a
+ * {@link ToApplicationChainReady} message has been sent to the web page), the
+ * web page can submit JSON-RPC requests and notifications to the chain client
+ * by sending {@link ToExtensionRpc} messages. The chain client sends back
+ * JSON-RPC responses and notifications using {@link ToApplicationRpc}
+ * messages.
+ *
+ * Once a web page no longer wants to interface with a certain chain, it should
+ * send a {@link ToExtensionRemoveChain} message to the extension in order for
+ * resources to be de-allocated.
+ *
+ * At any point in time, the extension can send a {@link ToApplicationError}
+ * message to indicate a critical problem with the chain or the extension that
+ * prevents execution from continuing. This can include for example the
+ * extension being disabled by the user, the underlying client crashing, a lack
+ * of memory, etc. If that happens, the web page is encouraged stop using the
+ * extension altogether.
+ *
+ * Note that if the extension sends a {@link ToApplicationError} message,
+ * either before of after the chain is ready, the corresponding `chainId` is
+ * immediately considered dead/removed, and the web page doesn't need to send
+ * a {@link ToExtensionRemoveChain} message.
  */
 
 interface ToApplicationHeader {
