97379f6a826b684b91e07a930db0b298ca44bee4
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-02-07 17:10:52 +0100

    Properly check type of received messages (#725)
    * Properly check type of received messages
    
    * Prettier
    
    * Fix lint and remove useless test
    
    * Doc
    
    * Also check unknown type

---
+++ projects/extension/src/content/checkMessage.ts
@@ -0,0 +1,48 @@
+import { ToExtension } from "@substrate/connect-extension-protocol"
+
+/**
+ * Checks whether a message sent on the `window` matches the `ToExtension` interface.
+ *
+ * Do not forget to update this function if the `ToExtension` interface changes!
+ */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export default function checkReceivedMessage(msg: any): msg is ToExtension {
+  const message = msg as ToExtension
+
+  if (message.origin !== "substrate-connect-client") return false
+  if (typeof message.type !== "string") return false
+
+  switch (message.type) {
+    case "add-chain": {
+      if (typeof message.chainId !== "string") return false
+      if (typeof message.chainSpec !== "string") return false
+      if (!Array.isArray(message.potentialRelayChainIds)) return false
+      for (const element of message.potentialRelayChainIds) {
+        if (typeof element !== "string") return false
+      }
+      break
+    }
+
+    case "add-well-known-chain": {
+      if (typeof message.chainId !== "string") return false
+      if (typeof message.chainName !== "string") return false
+      break
+    }
+
+    case "rpc": {
+      if (typeof message.chainId !== "string") return false
+      if (typeof message.jsonRpcMessage !== "string") return false
+      break
+    }
+
+    case "remove-chain": {
+      if (typeof message.chainId !== "string") return false
+      break
+    }
+
+    default:
+      return false
+  }
+
+  return true
+}
