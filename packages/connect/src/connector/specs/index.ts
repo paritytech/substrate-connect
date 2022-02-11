c7bb0b570274d41c2522bd46477a801c07a2c739
wirednkod <wirednkod@gmail.com>
2022-02-11 21:56:48 +0200

    Map specs filenames with chain Id in order to retrieve correct spec when extension is not used

--- packages/connect/src/connector/specs/index.ts
+++ packages/connect/src/connector/specs/index.ts
@@ -1,7 +1,14 @@
+const mapNameToId: Map<string, string> = new Map<string, string>([
+  ["polkadot", "polkadot"],
+  ["ksmcc3", "kusama"],
+  ["rococo_v1_13", "rococo"],
+  ["westend2", "westend"],
+])
+
 export async function getSpec(chain: string): Promise<string> {
-  const specRaw = (await import("./generated/" + chain + ".js")) as
-    | string
-    | { default: string }
+  const specRaw = (await import(
+    "./generated/" + mapNameToId.get(chain) + ".js"
+  )) as string | { default: string }
 
   return typeof specRaw === "string"
     ? specRaw
