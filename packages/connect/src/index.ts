01992f6fb8f1aa364bd1e3e923290524272d9a82
Nikos Kontakis <wirednkod@gmail.com>
2022-02-11 12:49:02 +0200

    Transfer health checker from Smoldot to Substrate Connect (#737)
    * Transfer HealthChecker and fix Typescript details
    
    * Move HealthCheckError with HealthCheck; link healthChecker with substrate-connect's healthchecker
    
    * change type to interface
    
    * Address comments
    
    * Address comments
    
    * Update packages/connect/src/ScProvider/Health.ts
    
    Co-authored-by: Pierre Krieger <pierre.krieger1708@gmail.com>
    
    Co-authored-by: Pierre Krieger <pierre.krieger1708@gmail.com>

--- packages/connect/src/index.ts
+++ packages/connect/src/index.ts
@@ -1,3 +1,3 @@
 export * from "./connector/index.js"
 export { WellKnownChains } from "./WellKnownChains.js"
-export { ScProvider } from "./ScProvider/index.js"
+export { ScProvider, healthChecker } from "./ScProvider/index.js"
