6d81fac9147c016f7ec023751a50496d5ffc386d
Josep M Sobrepere <jm.sobrepere@gmail.com>
2022-02-14 09:58:40 +0100

    Revert "different approach to fix the linting issue"
    This reverts commit 781d9d4cee6bf88d1ccf69cb37b120977d2d161b.

--- projects/burnr/.eslintrc.js
+++ projects/burnr/.eslintrc.js
@@ -1,10 +1,5 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  parserOptions: {
-    // See the connect .eslintrc.cjs for an explanation of this config
-    EXPERIMENTAL_useSourceOfProjectReferenceRedirect: true,
-    project: ["../../packages/connect/tsconfig.json", "./tsconfig.json"],
-  },
   rules: {
     "react/prop-types": 0,
     "react-hooks/rules-of-hooks": "error",
