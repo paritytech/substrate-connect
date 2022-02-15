497b27619400b862a879d384e21e2a8e2b2a5c03
wirednkod <wirednkod@gmail.com>
2022-02-15 08:57:37 +0200

    alter zombienet's scripts

--- .zombienet-tests/utils.js
+++ .zombienet-tests/utils.js
@@ -3,10 +3,9 @@ const polkadotApi = require("@polkadot/api")
 async function connect(nodeName, networkInfo) {
   const { userDefinedTypes } = networkInfo.nodesByName[nodeName]
   const customChainSpec = require(networkInfo.chainSpecPath)
-  const substrateConnect = await import("@substrate/connect")
-  const provider = new substrateConnect.ScProvider(
-    JSON.stringify(customChainSpec),
-  )
+  const { createScClient } = await import("@substrate/connect")
+  const scClient = createScClient()
+  const provider = scClient.addChain(JSON.stringify(customChainSpec))
   const api = new polkadotApi.ApiPromise({ provider, types: userDefinedTypes })
   return api
 }
