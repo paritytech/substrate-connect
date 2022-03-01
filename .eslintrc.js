88f63c63eb9b91640364ea7c3644f86fe774bca9
Josep M Sobrepere <jm.sobrepere@gmail.com>
2022-03-01 10:43:18 +0100

    chore: add import/no-extraneous-dependencies eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,13 @@ module.exports = {
     createDefaultProgram: true,
   },
   extends: ["react-app", "react-app/jest", "prettier"],
+  plugins: ["import"],
+  rules: {
+    "import/no-extraneous-dependencies": [
+      "error",
+      { devDependencies: ["**/*.test.ts", "**/*.spec.ts"] },
+    ],
+  },
   env: {
     browser: true,
   },
