36732254c6f66393a15e538f70af2ada44a2b0de
wirednkod <wirednkod@gmail.com>
2022-03-07 20:16:18 +0200

    Add parachain spec

--- .zombienet-tests/0002-placeholder.js
+++ .zombienet-tests/0002-placeholder.js
@@ -1,8 +1,8 @@
-const { connect } = require("./utils")
+const { connectParachain } = require("./utils")
 
 async function run(nodeName, networkInfo) {
   console.log(networkInfo)
-  const api = await connect(nodeName, networkInfo)
+  const api = await connectParachain(nodeName, networkInfo)
   let count = 0
   await new Promise(async (resolve, reject) => {
     const unsub = await api.rpc.chain.subscribeNewHeads((header) => {
--- .zombienet-tests/utils.js
+++ .zombienet-tests/utils.js
@@ -3,13 +3,23 @@ const { ApiPromise } = require("@polkadot/api")
 async function connect(nodeName, networkInfo) {
   const { userDefinedTypes } = networkInfo.nodesByName[nodeName]
   const customChainSpec = require(networkInfo.chainSpecPath)
-  const { createPolkadotJsScClient, WellKnownChain } = await import(
-    "@substrate/connect"
-  )
+  const { createPolkadotJsScClient } = await import("@substrate/connect")
   const scClient = createPolkadotJsScClient()
   const provider = await scClient.addChain(JSON.stringify(customChainSpec))
   const api = await ApiPromise.create({ provider, types: userDefinedTypes })
   return api
 }
 
-module.exports = { connect }
+async function connectParachain(nodeName, networkInfo) {
+  const { userDefinedTypes } = networkInfo.nodesByName[nodeName]
+  const customChainSpec = require(networkInfo.chainSpecPath)
+  const customParachainSpec = require(networkInfo.paras[100].spec)
+  const { createPolkadotJsScClient } = await import("@substrate/connect")
+  const scClient = createPolkadotJsScClient()
+  await scClient.addChain(JSON.stringify(customChainSpec))
+  const provider = await scClient.addChain(JSON.stringify(customParachainSpec))
+  const api = await ApiPromise.create({ provider, types: userDefinedTypes })
+  return api
+}
+
+module.exports = { connect, connectParachain }
