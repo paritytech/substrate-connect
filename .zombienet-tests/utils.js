9432271925e50b82b1da47e69cb6dd48e5e9e87b
wirednkod <wirednkod@gmail.com>
2022-03-07 23:43:18 +0200

    test parachain

--- .zombienet-tests/utils.js
+++ .zombienet-tests/utils.js
@@ -3,13 +3,11 @@ const { ApiPromise } = require("@polkadot/api")
 async function connect(nodeName, networkInfo, isParachain) {
   const { userDefinedTypes } = networkInfo.nodesByName[nodeName]
   const customChainSpec = require(networkInfo.chainSpecPath)
-  const { createPolkadotJsScClient, WellKnownChain } = await import(
-    "@substrate/connect"
-  )
+  const { createPolkadotJsScClient } = await import("@substrate/connect")
   const scClient = createPolkadotJsScClient()
   let provider
   if (isParachain) {
-    await scClient.addChain(WellKnownChain.rococo_v2)
+    await scClient.addChain(JSON.stringify(customChainSpec))
     const customParachainSpec = require(networkInfo?.paras[100]?.spec)
     provider = await scClient.addChain(JSON.stringify(customParachainSpec))
   } else {
