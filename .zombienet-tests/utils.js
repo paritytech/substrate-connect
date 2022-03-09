b63a8ee77cd643184fab17ca9f1837bf02db6635
Javier Viola <javier@parity.io>
2022-03-09 09:09:29 -0300

    bump zombienet and small changes

--- .zombienet-tests/utils.js
+++ .zombienet-tests/utils.js
@@ -1,14 +1,14 @@
 const { ApiPromise } = require("@polkadot/api")
 
-async function connect(nodeName, networkInfo, isParachain) {
+async function connect(nodeName, networkInfo, parachainId) {
   const { userDefinedTypes } = networkInfo.nodesByName[nodeName]
   const customChainSpec = require(networkInfo.chainSpecPath)
   const { createPolkadotJsScClient } = await import("@substrate/connect")
   const scClient = createPolkadotJsScClient()
   let provider
-  if (isParachain) {
+  if (parachainId) {
     await scClient.addChain(JSON.stringify(customChainSpec))
-    const customParachainSpec = require(networkInfo?.paras[100]?.spec)
+    const customParachainSpec = require(networkInfo?.paras[parachainId]?.chainSpecPath)
     provider = await scClient.addChain(JSON.stringify(customParachainSpec))
   } else {
     provider = await scClient.addChain(JSON.stringify(customChainSpec))
