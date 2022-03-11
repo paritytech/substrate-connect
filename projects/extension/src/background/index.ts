76e0234d288fa2fd779a734ca5c777b3050b8b13
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-03-11 14:55:08 +0100

    Remove isEmpty

--- projects/extension/src/background/index.ts
+++ projects/extension/src/background/index.ts
@@ -1,5 +1,4 @@
 import { ConnectionManagerWithHealth } from "./ConnectionManagerWithHealth"
-import { isEmpty } from "../utils/utils"
 import settings from "./settings.json"
 import { ExposedChainConnection } from "./types"
 import { WellKnownChain } from "@substrate/connect"
@@ -235,7 +234,7 @@ chrome.runtime.onConnect.addListener((port) => {
 })
 
 chrome.storage.local.get(["notifications"], (result) => {
-  if (isEmpty(result)) {
+  if (Object.keys(result).length === 0) {
     // Setup default settings
     chrome.storage.local.set({ notifications: settings.notifications }, () => {
       if (chrome.runtime.lastError) {
--- projects/extension/src/utils/utils.ts
+++
@@ -1,2 +0,0 @@
-export const isEmpty = (obj: Record<string, unknown>): boolean =>
-  obj && Object.keys(obj).length === 0 && obj.constructor === Object
