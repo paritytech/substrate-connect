f7d77c43c389055e456508c4d185e910584fdf55
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-03-16 12:31:58 +0100

    Improve well-known chain spec import

--- packages/connect/src/connector/specs/index.ts
+++ packages/connect/src/connector/specs/index.ts
@@ -1,9 +1,20 @@
 export async function getSpec(chain: string): Promise<string> {
-  const specRaw = (await import("./generated/" + chain + ".js")) as
-    | string
-    | { default: string }
+  // We don't want API users to be able to `import` a file outside of the `generated` directory.
+  // While it is probably harmless, better be safe than sorry.
+  // This is done by make sure that the name doesn't contain `..`. This also means that we can't
+  // support well-known chain whose name contains `..`, but that seems unlikely to ever be
+  // problematic.
+  if (chain.search("..") !== 1) throw new Error("Invalid chain name")
 
-  return typeof specRaw === "string"
-    ? specRaw
-    : (specRaw as unknown as { default: string }).default
+  try {
+    const specRaw = (await import("./generated/" + chain + ".js")) as
+      | string
+      | { default: string }
+
+    return typeof specRaw === "string"
+      ? specRaw
+      : (specRaw as unknown as { default: string }).default
+  } catch (error) {
+    throw new Error("Invalid chain name")
+  }
 }
