72fba5ea3690d4de077b3c63e2f612f3432028f6
Josep M Sobrepere <jm.sobrepere@gmail.com>
2022-03-28 19:57:28 +0200

    prettier...

--- .zombienet-tests/utils.js
+++ .zombienet-tests/utils.js
@@ -3,13 +3,18 @@ const { ApiPromise } = require("@polkadot/api")
 async function connect(nodeName, networkInfo, parachainId) {
   const { userDefinedTypes } = networkInfo.nodesByName[nodeName]
   const customChainSpec = require(networkInfo.chainSpecPath)
-  const { ScProvider } = await import("@polkadot/rpc-provider/substrate-connect")
+  const { ScProvider } = await import(
+    "@polkadot/rpc-provider/substrate-connect"
+  )
   let provider
   if (parachainId) {
     const relayProvider = new ScProvider(JSON.stringify(customChainSpec))
     const customParachainSpec = require(networkInfo?.paras[parachainId]
       ?.chainSpecPath)
-    provider = new ScProvider(JSON.stringify(customParachainSpec), relayProvider)
+    provider = new ScProvider(
+      JSON.stringify(customParachainSpec),
+      relayProvider,
+    )
   } else {
     provider = new ScProvider(JSON.stringify(customChainSpec))
   }
