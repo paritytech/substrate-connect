fa9ea215e14e028f978cdcd63683ab26d671fa71
wirednkod <wirednkod@gmail.com>
2022-04-07 23:21:15 +0300

    Update Popup to indicate crashed smoldot and reidect/reload buttons

--- projects/extension/src/containers/Popup.tsx
+++ projects/extension/src/containers/Popup.tsx
@@ -1,23 +1,46 @@
 import React, { FunctionComponent, useEffect, useRef, useState } from "react"
 import * as material from "@material-ui/core"
 import GlobalFonts from "../fonts/fonts"
+import { makeStyles } from "@material-ui/core/styles"
 import { light, MenuButton, Tab, Logo } from "../components"
 import CallMadeIcon from "@material-ui/icons/CallMade"
 import { Background } from "../background/"
 import { TabInterface } from "../types"
+import { Tooltip } from "@material-ui/core"
+import ErrorIcon from "@material-ui/icons/Error"
+import ReplayIcon from "@material-ui/icons/Replay"
 
 const { createTheme, ThemeProvider, Box, Divider } = material
 
+const useStyles = makeStyles(() => ({
+  flexBetween: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  replayIcon: {
+    width: "1.2rem",
+    marginLeft: "0.2rem",
+    color: "green",
+    cursor: "pointer",
+  },
+  errorIcon: { width: "1.2rem", color: "red", cursor: "pointer" },
+}))
+
 const Popup: FunctionComponent = () => {
+  const classes = useStyles()
   const disconnectTabRef = useRef<(tapId: number) => void>((_: number) => {})
   const [activeTab, setActiveTab] = useState<TabInterface | undefined>()
   const [otherTabs, setOtherTabs] = useState<TabInterface[]>([])
+  const [crashError, setCrashError] = useState<string>("")
   const appliedTheme = createTheme(light)
 
   useEffect(() => {
     let isActive = true
     let unsubscribe = () => {}
 
+    chrome.storage.local.get(["crashError"], (res) => {
+      setCrashError(res.crashError)
+    })
     ;(async () => {
       // retrieve open tabs and assign to local state
       const browserTabs = await new Promise<chrome.tabs.Tab[]>((res) =>
@@ -62,16 +85,35 @@ const Popup: FunctionComponent = () => {
     }
   }, [])
 
-  const goToOptions = (): void => {
-    chrome.runtime.openOptionsPage()
-  }
-
   return (
     <ThemeProvider theme={appliedTheme}>
       <Box width={"320px"} pl={1.5} pr={1.5}>
         <GlobalFonts />
         <Box pt={2} pb={1} pl={1} pr={1}>
-          <Logo />
+          {crashError ? (
+            <div className={classes.flexBetween}>
+              <div>
+                <Logo />
+              </div>
+              <div>
+                <ErrorIcon
+                  className={classes.errorIcon}
+                  onClick={() => chrome.runtime.openOptionsPage()}
+                />
+                <Tooltip
+                  placement="bottom"
+                  title={"Click for manual reload of the extension"}
+                >
+                  <ReplayIcon
+                    onClick={() => chrome.runtime.reload()}
+                    className={classes.replayIcon}
+                  />
+                </Tooltip>
+              </div>
+            </div>
+          ) : (
+            <Logo />
+          )}
         </Box>
 
         {activeTab && (
@@ -97,7 +139,7 @@ const Popup: FunctionComponent = () => {
             ))}
           </Box>
         )}
-        <MenuButton fullWidth onClick={goToOptions}>
+        <MenuButton fullWidth onClick={() => chrome.runtime.openOptionsPage()}>
           Options
         </MenuButton>
         <Divider />
