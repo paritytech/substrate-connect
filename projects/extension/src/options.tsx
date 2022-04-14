cbaf34479f782c3cdb34a19e4a616de007e2361b
wirednkod <wirednkod@gmail.com>
2022-04-15 02:35:14 +0300

    fix options css to work with tailwind

--- projects/extension/src/options.tsx
+++ projects/extension/src/options.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import Options from "./containers/Options"
+import "./style.css"
 
 ReactDOM.render(<Options />, document.getElementById("options"))
