80e67afcea721dc79342d6cea6e4db8f58ba2b31
wirednkod <wirednkod@gmail.com>
2022-04-16 00:37:23 +0300

    Remove unnecessery components

--- projects/extension/src/components/index.tsx
+++ projects/extension/src/components/index.tsx
@@ -1,6 +1,6 @@
 export { default as Logo } from "./Logo"
-export { default as IconWeb3 } from "./IconWeb3"
 export { default as StatusCircle } from "./StatusCircle"
 export { default as light } from "./theme"
 export { default as Tab } from "./Tab"
 export { default as NetworkTab } from "./NetworkTab"
+export { default as Loader } from "./Loader"
