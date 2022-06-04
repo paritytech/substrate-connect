9c2ec8e39883f770dad4fd773c9227c66520e819
Bernardo A. Rodrigues <bernardo@parity.io>
2022-06-04 14:09:59 -0300

    fix

--- projects/landing-page/src/App.tsx
+++ projects/landing-page/src/App.tsx
@@ -136,7 +136,7 @@ const App: React.FunctionComponent = () => {
             You have to provide a well-known chain name (polkadot, ksmcc3, westend2, rococo_v2_2).
             Note that these are the "real" names of the chains rather than the
             names they are more commonly known as (such as Kusama or Rococo). For example, "ksmcc3" is the name
-            for kusama. This is important for chains which have been hard forked. For example,
+            of Kusama. This is important for chains which have been hard forked. For example,
             "rococo_v2" and "rococo_v2_2" are two different chains.
             </SectionText>
           </Section>
