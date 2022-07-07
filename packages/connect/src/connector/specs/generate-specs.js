cac74668a715fe288e8b16a1573615d8f754f815
wirednkod <wirednkod@gmail.com>
2022-07-07 18:29:49 +0300

    Adjust generate-specs.js file to support both esm and cjs generation

--- packages/connect/src/connector/specs/generate-specs.js
+++ packages/connect/src/connector/specs/generate-specs.js
@@ -15,21 +15,24 @@ const BASE_PATH = "./src/connector/specs"
 
 const files = readdirSync(BASE_PATH)
 
+const type = process.argv.slice(2)
+
+if (!type || (type[0] !== "esm" && type[0] !== "cjs"))
+  throw new Error("Type `esm` or `cjs` must be provided.")
+const url = "./dist/" + type[0] + "/connector/specs/generated/"
+
 const processFile = async (fileName) => {
   const rawStr = await readFile(path.join(BASE_PATH, fileName), "utf8")
   const fileStr = `export default \`${JSON.stringify(JSON.parse(rawStr))}\``
   await writeFile(
-    `${path.join(
-      "./dist/esm/connector/specs/generated/",
-      fileName.slice(0, -5),
-    )}.js`,
+    `${path.join(url, fileName.slice(0, -5))}.js`,
     fileStr,
     "utf8",
   )
 }
 
-if (!existsSync("./dist/esm/connector/specs/generated")) {
-  mkdirSync("./dist/esm/connector/specs/generated", { resursive: true })
+if (!existsSync(url)) {
+  mkdirSync(url, { resursive: true })
 }
 
 Promise.all(files.filter((file) => file.endsWith(".json")).map(processFile))
