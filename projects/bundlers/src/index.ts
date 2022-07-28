10f79005aa551d474ff441362a761cd098f9da26
wirednkod <wirednkod@gmail.com>
2022-07-28 10:00:08 +0200

    fix linter

--- projects/bundlers/src/index.ts
+++ projects/bundlers/src/index.ts
@@ -1,6 +1,6 @@
 import "regenerator-runtime/runtime"
 import UI, { emojis } from "./view"
-import { createScClient, WellKnownChain } from "@substrate/connect";
+import { createScClient, WellKnownChain } from "@substrate/connect"
 
 window.onload = () => {
   const loadTime = performance.now()
@@ -8,24 +8,22 @@ window.onload = () => {
   void (async () => {
     try {
       const start = async () => {
-        const scClient = createScClient();
+        const scClient = createScClient()
         const westendChain = await scClient.addWellKnownChain(
           WellKnownChain.westend2,
           function jsonRpcCallback(response) {
             ui.log(response)
-          }
-        );
+          },
+        )
         westendChain.sendJsonRpc(
           '{"jsonrpc":"2.0","id":"1","method":"chainHead_unstable_follow","params":[true]}',
-        );
+        )
       }
-      start();
+      start()
       ui.log(`${emojis.seedling} Light client ready`)
-      ui.log(
-        `${emojis.info} Connected to Westend`,
-      )
+      ui.log(`${emojis.info} Connected to Westend`)
     } catch (error) {
-      ui.error(<Error>error)
+      ui.error(error as Error)
     }
   })()
 }
--- projects/bundlers/webpack.config.js
+++ projects/bundlers/webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
     fallback: {
       crypto: require.resolve("crypto-browserify"),
       stream: require.resolve("stream-browserify"),
-      buffer: require.resolve("buffer")
+      buffer: require.resolve("buffer"),
     },
   },
   plugins: [
