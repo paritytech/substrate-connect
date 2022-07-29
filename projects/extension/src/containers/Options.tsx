844dacf4043718cebb7c02887dc665e84ba2d8b4
wirednkod <wirednkod@gmail.com>
2022-07-29 18:29:36 +0200

    Add modal in Options

--- projects/extension/src/containers/Options.tsx
+++ projects/extension/src/containers/Options.tsx
@@ -5,6 +5,7 @@ import { Background } from "../background/"
 
 import { NetworkTabProps } from "../types"
 import { TabsContent } from "../components/Tabs"
+import { BraveModal } from "../components/BraveModal"
 
 interface logStructure {
   unix_timestamp: number
@@ -20,8 +21,8 @@ const Options: React.FunctionComponent = () => {
   const [warnLogs, setWarnLogs] = useState<logStructure[]>([])
   const [errLogs, setErrLogs] = useState<logStructure[]>([])
   const [poolingLogs, setPoolingLogs] = useState<boolean>(true)
-
   const [activeTab, setActiveTab] = useState<number>(0)
+  const [showModal, setShowModal] = useState<boolean>(false)
 
   const getTime = (d: number) => {
     const date = new Date(d)
@@ -64,8 +65,14 @@ const Options: React.FunctionComponent = () => {
   }, [poolingLogs])
 
   useEffect(() => {
-    chrome.storage.local.get(["notifications"], (res) => {
-      setNotifications(res.notifications as SetStateAction<boolean>)
+    window.navigator?.brave?.isBrave().then((isBrave: any) => {
+      chrome.storage.local.get(["braveSetting"], ({ braveSetting }) => {
+        setShowModal(isBrave && !braveSetting)
+      })
+    })
+
+    chrome.storage.local.get(["notifications"], ({ notifications }) => {
+      setNotifications(notifications as SetStateAction<boolean>)
     })
 
     let cb: () => void = () => {}
@@ -136,102 +143,110 @@ const Options: React.FunctionComponent = () => {
   }
 
   return (
-    <div className="mb-4 font-roboto">
-      <div className="options-container">
-        <div className="px-12 pb-3.5 text-base flex items-center">
-          <div className="flex items-baseline">
-            <Logo textSize="lg" />
-            <div className="text-sm pl-4">v{pckg.version}</div>
-          </div>
-          <div className="w-full ml-[10%]">
-            <Tabs
-              setActiveTab={(n: number) => setActiveTab(n)}
-              tabTitles={["Networks", "Logs"]}
-            />
+    <>
+      <BraveModal show={showModal} isOptions={true} />
+      <div className="mb-4 font-roboto">
+        <div className="options-container">
+          <div className="px-12 pb-3.5 text-base flex items-center">
+            <div className="flex items-baseline">
+              <Logo textSize="lg" />
+              <div className="text-sm pl-4">v{pckg.version}</div>
+            </div>
+            <div className="w-full ml-[10%]">
+              <Tabs
+                setActiveTab={(n: number) => setActiveTab(n)}
+                tabTitles={["Networks", "Logs"]}
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="mx-[15%] pt-2">
-        {/** Networks section */}
-        <TabsContent activeTab={activeTab}>
-          <section>
-            {networks.length ? (
-              networks.map((network: NetworkTabProps, i: number) => {
-                const { name, health, apps } = network
-                return (
-                  <NetworkTab key={i} name={name} health={health} apps={apps} />
-                )
-              })
-            ) : (
-              <div>No networks or apps are connected to the extension.</div>
-            )}
-          </section>
-          {/** Logs section */}
-          <section className="block">
-            <div className="flex my-5">
-              <button
-                className="px-2 border rounded-md bg-stone-200 hover:bg-stone-400"
-                onClick={() => setPoolingLogs(!poolingLogs)}
-              >
-                {poolingLogs ? "Pause" : "Retrieve "} logs
-              </button>
-              <button
-                className="px-2 mx-2 my-0 border rounded-md bg-stone-200 hover:bg-stone-400"
-                onClick={() => navigator.clipboard.writeText(stringifyLogs())}
-              >
-                Copy to clipboard
-              </button>
-              <div className="rounded-md bg-red-500 py-2.5 px-4">
-                {errLogs.length} Errors
-              </div>
-              <div className="ml-2 rounded-md bg-yellow-300 py-2.5 px-4">
-                {warnLogs.length} Warnings
-              </div>
-            </div>
-            <div
-              style={{ maxHeight: "75vh" }}
-              className="block w-full overflow-y-auto px-2 bg-black text-white text-xs border border-black font-mono font"
-            >
-              {allLogs.length > 0 ? (
-                allLogs.map(
-                  (
-                    { unix_timestamp, level, target, message }: logStructure,
-                    i: number,
-                  ) => (
-                    <p
-                      key={"all_" + i}
-                      style={{
-                        color: getLevelInfo(level)[1],
-                      }}
-                    >
-                      <span
-                        style={{
-                          fontWeight: "bold",
-                        }}
-                      >
-                        [{getTime(unix_timestamp)}]
-                      </span>
-                      <span
-                        style={{
-                          margin: "0 0.5rem",
-                        }}
-                      >
-                        [{target}]
-                      </span>
-                      <span>{message}</span>
-                    </p>
-                  ),
-                )
+        <div className="mx-[15%] pt-2">
+          {/** Networks section */}
+          <TabsContent activeTab={activeTab}>
+            <section>
+              {networks.length ? (
+                networks.map((network: NetworkTabProps, i: number) => {
+                  const { name, health, apps } = network
+                  return (
+                    <NetworkTab
+                      key={i}
+                      name={name}
+                      health={health}
+                      apps={apps}
+                    />
+                  )
+                })
               ) : (
-                <div className="items-center h-56 mt-20 ml-40">
-                  <Loader />
-                </div>
+                <div>No networks or apps are connected to the extension.</div>
               )}
-            </div>
-          </section>
-        </TabsContent>
+            </section>
+            {/** Logs section */}
+            <section className="block">
+              <div className="flex my-5">
+                <button
+                  className="px-2 border rounded-md bg-stone-200 hover:bg-stone-400"
+                  onClick={() => setPoolingLogs(!poolingLogs)}
+                >
+                  {poolingLogs ? "Pause" : "Retrieve "} logs
+                </button>
+                <button
+                  className="px-2 mx-2 my-0 border rounded-md bg-stone-200 hover:bg-stone-400"
+                  onClick={() => navigator.clipboard.writeText(stringifyLogs())}
+                >
+                  Copy to clipboard
+                </button>
+                <div className="rounded-md bg-red-500 py-2.5 px-4">
+                  {errLogs.length} Errors
+                </div>
+                <div className="ml-2 rounded-md bg-yellow-300 py-2.5 px-4">
+                  {warnLogs.length} Warnings
+                </div>
+              </div>
+              <div
+                style={{ maxHeight: "75vh" }}
+                className="block w-full overflow-y-auto px-2 bg-black text-white text-xs border border-black font-mono font"
+              >
+                {allLogs.length > 0 ? (
+                  allLogs.map(
+                    (
+                      { unix_timestamp, level, target, message }: logStructure,
+                      i: number,
+                    ) => (
+                      <p
+                        key={"all_" + i}
+                        style={{
+                          color: getLevelInfo(level)[1],
+                        }}
+                      >
+                        <span
+                          style={{
+                            fontWeight: "bold",
+                          }}
+                        >
+                          [{getTime(unix_timestamp)}]
+                        </span>
+                        <span
+                          style={{
+                            margin: "0 0.5rem",
+                          }}
+                        >
+                          [{target}]
+                        </span>
+                        <span>{message}</span>
+                      </p>
+                    ),
+                  )
+                ) : (
+                  <div className="items-center h-56 mt-20 ml-40">
+                    <Loader />
+                  </div>
+                )}
+              </div>
+            </section>
+          </TabsContent>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
