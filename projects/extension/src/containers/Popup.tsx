13e876dcadc42ed4f2de22ec314b818675d245d4
wirednkod <wirednkod@gmail.com>
2022-09-21 11:44:01 +0300

    Fix links to substrate.io

--- projects/extension/src/containers/Popup.tsx
+++ projects/extension/src/containers/Popup.tsx
@@ -221,15 +221,13 @@ const Popup: FunctionComponent = () => {
           <button
             className="font-inter flex w-full justify-between py-3.5 text-sm font-light capitalize"
             onClick={() =>
-              window.open(
-                "https://paritytech.github.io/substrate-connect/#extension",
-              )
+              window.open("https://substrate.io/developers/substrate-connect/")
             }
           >
             <div className="text-lg font-inter font-normal">About</div>
             <div className="tooltip">
               <span className="p-4 text-xs shadow-lg tooltiptext tooltip_left">
-                Go to Landing Page
+                Go to Substrate.io - Substrate-connect
               </span>
               <MdOutlineEast className="text-xl" />
             </div>
