8fc28266afcb5a74f8285d7e4bcc4315d86a7c54
Nikos Kontakis <wirednkod@gmail.com>
2022-09-21 09:03:35 +0300

    Update projects/extension/src/containers/Popup.tsx
    Co-authored-by: Pierre Krieger <pierre.krieger1708@gmail.com>

--- projects/extension/src/containers/Popup.tsx
+++ projects/extension/src/containers/Popup.tsx
@@ -201,7 +201,7 @@ const Popup: FunctionComponent = () => {
                       </span>
                     </div>
                     <div className="pl-16 flex pt-2">
-                      <span className="text-[#323232]">Last block</span>
+                      <span className="text-[#323232]">Latest block</span>
                       <span className="pl-2 text-[#24CC85]">
                         {w?.details[0].bestBlockHeight?.toLocaleString(
                           "en-US",
