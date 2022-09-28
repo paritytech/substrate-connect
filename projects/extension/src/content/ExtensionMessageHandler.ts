b0e7fbdc2d2acb8b21db2cfe9012a343662ea1bc
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-09-28 18:54:53 +0200

    Run smoldot in the content script instead of the extension (#1272)
    * ExtensionMessageRouter -> Handler
    
    * ExtensionMessageRouter -> Handler
    
    * First draft with new content script
    
    * Better logs
    
    * Adjust the background to make it work
    
    * Remove now-useless modules
    
    * Minor clean ups
    
    * Report info about the chains to the extension
    
    * Add comment
    
    * Small code tweaks
    
    * Add extern: in front of requests
    
    * Send chain updates to the extension
    
    * Properly support potentialRelayChains
    
    * Simplify onSmoldotCrashErrorChanged
    
    * Add TODO for database messages
    
    * Add tab information
    
    * Add saving and reloading database
    
    * Send periodic health requests
    
    * Send databases to the extension
    
    * Increase timer for database
    
    * Remove TODO and unused imports
    
    * Clean up assert
    
    * Only ask for DB is well known
    
    * Prettier
    
    * Somehow this file didn't go through prettier
    
    * Lint
    
    * I don't know what to do with these tests

---
+++ projects/extension/src/content/ExtensionMessageHandler.ts
@@ -0,0 +1,147 @@
+import { ToApplication } from "@substrate/connect-extension-protocol"
+import checkMessage from "./checkMessage"
+
+import {
+  SmoldotClientWithExtension,
+  ChainWithExtension,
+} from "./ClientWithExtension"
+
+const EXTENSION_PROVIDER_ORIGIN = "substrate-connect-client"
+
+const sendMessage = (msg: ToApplication): void => {
+  window.postMessage(msg, "*")
+}
+
+export class ExtensionMessageHandler {
+  #clientWithExtension?: SmoldotClientWithExtension
+  #chains: Map<string, ChainWithExtension> = new Map()
+
+  /**
+   * connections returns the names of all the ports this `ExtensionMessageRouter`
+   * is managing for the app.
+   *
+   * @returns A list of strings
+   */
+  get connections(): string[] {
+    return [...this.#chains.keys()]
+  }
+
+  /** listen starts listening for messages sent by an app.  */
+  listen(): void {
+    window.addEventListener("message", this.#handleMessage)
+  }
+
+  /** stop stops listening for messages sent by apps.  */
+  stop(): void {
+    window.removeEventListener("message", this.#handleMessage)
+  }
+
+  #handleMessage = (msg: MessageEvent): void => {
+    const data = msg.data
+    if (!data.origin || data.origin !== EXTENSION_PROVIDER_ORIGIN) {
+      return
+    }
+
+    if (!checkMessage(data)) {
+      // probably someone abusing the extension
+      console.warn("Malformed message - unrecognised message.type", data)
+      return
+    }
+
+    if (!this.#clientWithExtension) {
+      this.#clientWithExtension = new SmoldotClientWithExtension()
+    }
+
+    // TODO: must handles smoldot crashes
+
+    switch (data.type) {
+      case "rpc": {
+        const chain = this.#chains.get(data.chainId)
+
+        // If the chainId is invalid, the message is silently discarded, as documented.
+        if (!chain) return
+
+        chain.sendJsonRpc(data.jsonRpcMessage)
+        break
+      }
+
+      case "add-chain":
+      case "add-well-known-chain": {
+        if (this.#chains.has(data.chainId)) {
+          sendMessage({
+            origin: "substrate-connect-extension",
+            type: "error",
+            chainId: data.chainId,
+            errorMessage: "ChainId already in use",
+          })
+          return
+        }
+
+        const jsonRpcCallback = (jsonRpcMessage: string) => {
+          console.assert(this.#chains.has(data.chainId))
+          sendMessage({
+            origin: "substrate-connect-extension",
+            type: "rpc",
+            chainId: data.chainId,
+            jsonRpcMessage,
+          })
+        }
+
+        const potentialRelayChains =
+          data.type !== "add-chain"
+            ? []
+            : data.potentialRelayChainIds
+                .filter((c) => this.#chains.has(c))
+                .map((c) => this.#chains.get(c)!)
+
+        let createChainPromise
+        if (data.type === "add-well-known-chain") {
+          createChainPromise = this.#clientWithExtension.addWellKnownChain({
+            chainName: data.chainName,
+            jsonRpcCallback,
+            potentialRelayChains,
+          })
+        } else {
+          createChainPromise = this.#clientWithExtension.addChain({
+            chainSpec: data.chainSpec,
+            jsonRpcCallback,
+            potentialRelayChains,
+          })
+        }
+
+        createChainPromise.then(
+          (chain) => {
+            this.#chains.set(data.chainId, chain)
+            sendMessage({
+              origin: "substrate-connect-extension",
+              type: "chain-ready",
+              chainId: data.chainId,
+            })
+          },
+          (error) => {
+            sendMessage({
+              origin: "substrate-connect-extension",
+              type: "error",
+              chainId: data.chainId,
+              errorMessage: error.toString(),
+            })
+          },
+        )
+
+        break
+      }
+
+      case "remove-chain": {
+        const chain = this.#chains.get(data.chainId)
+
+        // If the chainId is invalid, the message is silently discarded, as documented.
+        if (!chain) return
+
+        chain.remove()
+        this.#chains.delete(data.chainId)
+
+        break
+      }
+    }
+  }
+}
