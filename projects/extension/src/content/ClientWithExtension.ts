f8c05396d80bd4b01e4ae0069dadc2a8fc068cc7
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-10-04 09:04:56 +0200

    Fix wrong parameter sent to chainHead_unstable_follow

--- projects/extension/src/content/ClientWithExtension.ts
+++ projects/extension/src/content/ClientWithExtension.ts
@@ -311,7 +311,7 @@ export class SmoldotClientWithExtension {
                   jsonrpc: "2.0",
                   id: "ready-sub:" + this.#nextRpcRqId,
                   method: "chainHead_unstable_follow",
-                  params: [true],
+                  params: [false],
                 }),
               )
               this.#nextRpcRqId += 1
