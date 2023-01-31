4fcc0f678bf8674bbd528b30b12b20ed936b679d
wirednkod <wirednkod@gmail.com>
2023-01-31 16:58:32 -0300

    Fix zombienet tests

--- .zombienet-tests/utils.js
+++ .zombienet-tests/utils.js
@@ -1,3 +1,4 @@
+const Sc = require("@substrate/connect")
 const { ApiPromise } = require("@polkadot/api")
 
 async function connect(nodeName, networkInfo, parachainId) {
@@ -5,18 +6,19 @@ async function connect(nodeName, networkInfo, parachainId) {
   const customChainSpec = require(networkInfo.chainSpecPath)
   const { ScProvider } = await import(
     "@polkadot/rpc-provider/substrate-connect"
-  )
+  );
   let provider
   if (parachainId) {
-    const relayProvider = new ScProvider(JSON.stringify(customChainSpec))
+    const relayProvider = new ScProvider(Sc, JSON.stringify(customChainSpec))
     const customParachainSpec = require(networkInfo?.paras[parachainId]
       ?.chainSpecPath)
     provider = new ScProvider(
+      Sc,
       JSON.stringify(customParachainSpec),
       relayProvider,
     )
   } else {
-    provider = new ScProvider(JSON.stringify(customChainSpec))
+    provider = new ScProvider(Sc, JSON.stringify(customChainSpec))
   }
   await provider.connect()
   return ApiPromise.create({ provider, types: userDefinedTypes })
