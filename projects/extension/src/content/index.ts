4d1692b19e662d2b0da47cd3e84857cf0f098ff3
Matias Volpe <matias@parity.io>
2023-08-16 09:12:41 -0300

    feat: handle Chrome prerendering

--- projects/extension/src/content/index.ts
+++ projects/extension/src/content/index.ts
@@ -1,11 +1,14 @@
-import { ExtensionMessageHandler } from "./ExtensionMessageHandler"
 import {
   DOM_ELEMENT_ID,
   ToApplication,
 } from "@substrate/connect-extension-protocol"
 import { ToExtension } from "../background/protocol"
+import checkMessage from "./checkMessage"
+
+const EXTENSION_PROVIDER_ORIGIN = "substrate-connect-client"
 
 chrome.runtime.onMessage.addListener((msg: ToApplication) => {
+  console.log("message from background")
   window.postMessage(msg, "*")
 })
 
@@ -14,21 +17,46 @@ chrome.runtime.onMessage.addListener((msg: ToApplication) => {
 // indicating that the state of that content script has been reset.
 chrome.runtime.sendMessage({ type: "tab-reset" } as ToExtension)
 
-const router = new ExtensionMessageHandler()
+// Set up a promise for when the page is activated,
+// which is needed for prerendered pages.
+const whenActivated = new Promise<void>((resolve) => {
+  // @ts-ignore
+  if (document.prerendering) {
+    // @ts-ignore
+    document.addEventListener("prerenderingchange", resolve)
+  } else {
+    resolve()
+  }
+})
 
 // inject as soon as possible the DOM element necessary for web pages to know that the extension
 // is available
 window.document.addEventListener("readystatechange", () => {
-  if (window.document.readyState === "interactive") {
-    // If there is already a DOM element inject, don't create another element and don't listen.
-    // This way, if multiple extensions are installed, only one will actually be active on any
-    // given page.
-    if (document.getElementById(DOM_ELEMENT_ID) !== null) return
+  if (window.document.readyState !== "interactive") return
 
-    const s = document.createElement("span")
-    s.id = DOM_ELEMENT_ID
-    s.setAttribute("style", "display:none")
-    document.body.appendChild(s)
-    router.listen()
-  }
+  // If there is already a DOM element inject, don't create another element and don't listen.
+  // This way, if multiple extensions are installed, only one will actually be active on any
+  // given page.
+  if (document.getElementById(DOM_ELEMENT_ID) !== null) return
+
+  const s = document.createElement("span")
+  s.id = DOM_ELEMENT_ID
+  s.setAttribute("style", "display:none")
+  document.body.appendChild(s)
+
+  window.addEventListener("message", async ({ data }) => {
+    if (!data.origin || data.origin !== EXTENSION_PROVIDER_ORIGIN) {
+      return
+    }
+
+    if (!checkMessage(data)) {
+      // probably someone abusing the extension
+      console.warn("Malformed message - unrecognised message.type", data)
+      return
+    }
+
+    await whenActivated
+
+    chrome.runtime.sendMessage(data)
+  })
 })
