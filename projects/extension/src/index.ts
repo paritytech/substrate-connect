543e9f9b568941ef5d6eea83f8558f3c2abb5fb7
Matias Volpe <kratico@users.noreply.github.com>
2023-09-14 08:19:06 -0300

    fix: typedoc configuration (#1545)
    * fix: typedoc configuration
    
    * fix: typedoc references
    
    * chore: add usage with a worker
    
    * chore: document workerFactory

--- projects/extension/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { ConnectionManager } from "./background/ConnectionManager"
-import { ExtensionMessageHandler } from "./content/ExtensionMessageHandler"
-
-// This file is a fake entrypoint that exports the documented parts of the code
-// so that typedoc can discover them to generate the documentation.  Don't
-// remove it or we'll documentation generation will fail.
-
-export { ConnectionManager, ExtensionMessageHandler as ExtensionMessageRouter }
