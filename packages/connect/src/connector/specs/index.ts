34583a5e61882469a962dcbada18d77f491bf2cd
Matias Volpe <matias@parity.io>
2023-09-26 12:56:54 -0300

    feat: use static dynamic imports for Parcel bundler

--- packages/connect/src/connector/specs/index.ts
+++ packages/connect/src/connector/specs/index.ts
@@ -1,7 +1,17 @@
 import { WellKnownChain } from "../../WellKnownChain.js"
 
+let chains: Record<WellKnownChain, Promise<{ chainSpec: string }>> | undefined
+
 export async function getSpec(chain: string): Promise<string> {
   if (!Object.keys(WellKnownChain).includes(chain))
     throw new Error("Invalid chain name")
-  return (await import(`./js/${chain}.js`)).chainSpec
+  if (!chains) {
+    chains = {
+      polkadot: import("./js/polkadot.js"),
+      ksmcc3: import("./js/ksmcc3.js"),
+      westend2: import("./js/westend2.js"),
+      rococo_v2_2: import("./js/rococo_v2_2.js"),
+    }
+  }
+  return (await chains[chain as WellKnownChain]).chainSpec
 }
