1049bdf6b6d9140b30e71baf3c0dc362438e8c49
Matias Volpe <matias@parity.io>
2023-09-26 11:07:55 -0300

    feat: use named export for @substrate/connect chainspecs

--- packages/connect/src/connector/specs/index.ts
+++ packages/connect/src/connector/specs/index.ts
@@ -3,5 +3,5 @@ import { WellKnownChain } from "../../WellKnownChain.js"
 export async function getSpec(chain: string): Promise<string> {
   if (!Object.keys(WellKnownChain).includes(chain))
     throw new Error("Invalid chain name")
-  return (await import(`./js/${chain}.js`)).default
+  return (await import(`./js/${chain}.js`)).chainSpec
 }
