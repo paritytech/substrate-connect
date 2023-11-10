b57bf4cc1310d4f8de38a7dd2838c3fbe7f77683
Matias <kratico@users.noreply.github.com>
2023-11-10 09:23:23 -0300

    chore: increase smoldot log level

--- projects/extension/src/background/index.ts
+++ projects/extension/src/background/index.ts
@@ -1,4 +1,4 @@
 import { start } from "smoldot"
 import { register } from "@polkadot-api/light-client-extension-helpers/background"
 
-register(start({ maxLogLevel: 3 }))
+register(start({ maxLogLevel: 4 }))
