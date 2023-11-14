c9d3ebe42234597b21683377afae4c220060225e
Matias <kratico@users.noreply.github.com>
2023-11-14 10:25:39 -0300

    feat: improve getClientAndIncRef logic

--- projects/extension/src/background/addChain.ts
+++ projects/extension/src/background/addChain.ts
@@ -10,17 +10,13 @@ import {
 let clientReferences: number = 0
 let client: Client | null = null
 const getClientAndIncRef = () => {
-  if (client) {
-    clientReferences++
-    return client
-  }
-
-  client = start({
-    forbidTcp: true, // In order to avoid confusing inconsistencies between browsers and NodeJS, TCP connections are always disabled.
-    forbidNonLocalWs: true, // Prevents browsers from emitting warnings if smoldot tried to establish non-secure WebSocket connections
-    maxLogLevel: 3,
-    cpuRateLimit: 0.5, // Politely limit the CPU usage of the smoldot background worker.
-  })
+  if (!client)
+    client = start({
+      forbidTcp: true, // In order to avoid confusing inconsistencies between browsers and NodeJS, TCP connections are always disabled.
+      forbidNonLocalWs: true, // Prevents browsers from emitting warnings if smoldot tried to establish non-secure WebSocket connections
+      maxLogLevel: 3,
+      cpuRateLimit: 0.5, // Politely limit the CPU usage of the smoldot background worker.
+    })
 
   clientReferences++
   return client
