f76c0e6b63125d50611df1a50a6f1c211ffcf4e6
Matias <kratico@users.noreply.github.com>
2023-11-21 15:24:29 -0300

    feat: use helper bootnodes feature

--- projects/extension/src/hooks/useActiveChains.ts
+++ projects/extension/src/hooks/useActiveChains.ts
@@ -29,17 +29,7 @@ import {
 } from "rxjs"
 import { fromHex } from "@polkadot-api/utils"
 import { compact } from "scale-ts"
-
-const wellKnownChainIdByGenesisHash: Record<string, string> = {
-  "0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3":
-    "polkadot",
-  "0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe":
-    "ksmcc3",
-  "0xe143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e":
-    "westend2",
-  "0x6408de7737c59c238890533af25896a2c20608d8b380bb01029acb392781063e":
-    "rococo_v2_2",
-}
+import { wellKnownChainIdByGenesisHash } from "../constants"
 
 type Chain = {
   genesisHash: string
