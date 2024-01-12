0db57dbb9a6279083a371a9e33309ff1a2bcc225
Matias <kratico@users.noreply.github.com>
2024-01-12 16:37:39 -0300

    feat: use rpc in web-page-helper

--- packages/light-client-extension-helpers/src/web-page/web-page-helper.ts
+++ packages/light-client-extension-helpers/src/web-page/web-page-helper.ts
@@ -1,16 +1,12 @@
-import type {
-  PostMessage,
-  ToExtension,
-  ToExtensionRequest,
-  ToPage,
-  ToPageResponse,
-} from "@/protocol"
+import type { PostMessage, ToExtension, ToPage } from "@/protocol"
 import {
   CONTEXT,
   createBackgroundClientConnectProvider,
   createIsHelperMessage,
+  createRpc,
 } from "@/shared"
-import type { LightClientProvider, RawChain } from "./types"
+import type { LightClientProvider, RawChain, WebPageRpcHandlers } from "./types"
+import type { BackgroundRpcHandlers } from "@/background/types"
 
 export type * from "./types"
 
@@ -28,86 +24,62 @@ const channelIds = new Set<string>()
 export const getLightClientProvider = async (
   channelId: string,
 ): Promise<LightClientProvider> => {
-  const chainsChangeCallbacks: Parameters<
-    LightClientProvider["addChainsChangeListener"]
-  >[0][] = []
   if (channelIds.has(channelId))
     throw new Error(`channelId "${channelId}" already in use`)
   channelIds.add(channelId)
 
-  const pendingRequests: Record<
-    string,
-    { resolve(result: any): void; reject(error: string): void }
-  > = {}
-
-  window.addEventListener("message", ({ data, source }) => {
-    if (source !== window || !data) return
-    const { channelId: messageChannelId, msg } = data
-    if (messageChannelId !== channelId) return
-    if (!isHelperMessage(msg)) return
-    if (msg.origin === "substrate-connect-extension")
-      return rawChainCallbacks.forEach((cb) => cb(msg))
-    if (msg.origin === CONTEXT.CONTENT_SCRIPT) {
-      const pendingRequest = pendingRequests[msg.id]
-      if (!pendingRequest) return console.warn("Unhandled response", msg)
-      msg.type === "error"
-        ? pendingRequest.reject(msg.error)
-        : pendingRequest.resolve(msg)
-      delete pendingRequests[msg.id]
-      return
-    }
-    if (msg.origin === CONTEXT.BACKGROUND && msg.type === "onAddChains")
-      return chainsChangeCallbacks.forEach((cb) =>
+  const chainsChangeCallbacks: Parameters<
+    LightClientProvider["addChainsChangeListener"]
+  >[0][] = []
+  const handlers: WebPageRpcHandlers = {
+    onAddChains(chains) {
+      chainsChangeCallbacks.forEach((cb) =>
         cb(
           Object.fromEntries(
-            Object.entries(msg.chains).map(([key, { genesisHash, name }]) => [
+            Object.entries(chains).map(([key, { genesisHash, name }]) => [
               key,
               createRawChain(channelId, { genesisHash, name }),
             ]),
           ),
         ),
       )
+    },
+  }
+  const rpc = createRpc<BackgroundRpcHandlers>(
+    (msg) =>
+      window.postMessage(
+        { channelId, msg: { origin: CONTEXT.WEB_PAGE, ...msg } },
+        window.origin,
+      ),
+    handlers,
+  )
+
+  window.addEventListener("message", ({ data, source }) => {
+    if (source !== window || !data) return
+    const { channelId: messageChannelId, msg } = data
+    if (messageChannelId !== channelId) return
+    if (!isHelperMessage(msg)) return
+    // FIXME: isInternalRpcMessage()
+    if (msg.origin !== "substrate-connect-extension")
+      return rpc.handle(msg as any)
+    // FIXME: isSubstrateConnectMessage()
+    if (msg.origin === "substrate-connect-extension")
+      return rawChainCallbacks.forEach((cb) => cb(msg))
 
     console.warn("Unhandled message", msg)
   })
 
-  const requestReply = <
-    TReq extends ToExtensionRequest,
-    TRes extends ToPageResponse & {
-      type: `${TReq["type"]}Response`
-    },
-  >(
-    channelId: string,
-    msg: TReq,
-  ): Promise<TRes> => {
-    const promise = new Promise<TRes>((resolve, reject) => {
-      pendingRequests[msg.id] = { resolve, reject }
-    })
-    postToExtension({ channelId, msg })
-    return promise
-  }
+  // FIXME: rename isBackgroundScriptReady
+  await rpc.call("isBackgroundScriptReady", [])
 
-  // FIXME: improve id generation, random-id?
-  const nextId = (
-    (initialId) => () =>
-      "" + initialId++
-  )(0)
-
-  let { chains } = await requestReply(channelId, {
-    origin: CONTEXT.WEB_PAGE,
-    id: nextId(),
-    type: "getChains",
-  })
+  let chains = await rpc.call("getChains", [])
   chainsChangeCallbacks.push((chains_) => (chains = chains_))
   return {
     async getChain(chainSpec, relayChainGenesisHash) {
-      const { chain: chainInfo } = await requestReply(channelId, {
-        origin: CONTEXT.WEB_PAGE,
-        id: nextId(),
-        type: "getChain",
+      const chainInfo = await rpc.call("getChain", [
         chainSpec,
         relayChainGenesisHash,
-      })
+      ])
       return createRawChain(
         channelId,
         chains[chainInfo.genesisHash]
