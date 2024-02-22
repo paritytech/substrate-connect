ff1ff1936a6a686c616148d5dfebf763fbd77167
Ryan Lee <ryan@parity.io>
2024-02-22 02:08:27 -0500

    feat: secure storage

---
+++ projects/wallet/vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vitest/config"
+import { WxtVitest } from "wxt/testing"
+
+export default defineConfig({
+  plugins: [WxtVitest()],
+  ssr: {
+    noExternal: ["@webext-core/storage"],
+  },
+})
