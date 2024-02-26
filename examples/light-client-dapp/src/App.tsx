c2c3bfc15d2dde170790ef31eb1db420534151e8
Ryan Lee <ryan@parity.io>
2024-02-25 20:54:52 -0500

    wip

--- examples/light-client-dapp/src/App.tsx
+++ examples/light-client-dapp/src/App.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react"
 import { Transfer } from "./components/Transfer"
+import {
+  Account,
+  getLegacyProvider,
+} from "@polkadot-api/legacy-polkadot-provider"
+import { createScClient } from "@substrate/connect"
 
 function App() {
+  const [{ relayChains, connectAccounts }, _2] = useState(
+    getLegacyProvider(createScClient()),
+  )
+  const [accounts, setAccounts] = useState<Account[]>([])
+
+  useEffect(() => {
+    ;(async () => {
+      await connectAccounts("polkadot-js")
+      const accounts = await relayChains.westend2.getAccounts()
+      setAccounts(accounts)
+    })()
+  }, [connectAccounts, relayChains])
+
   return (
     <main className="container" style={{ maxWidth: "700px" }}>
       <h1>Light Client DApp</h1>
-      <Transfer />
+      <Transfer accounts={accounts} />
     </main>
   )
 }
--- examples/light-client-dapp/src/components/Transfer.tsx
+++ examples/light-client-dapp/src/components/Transfer.tsx
@@ -1,28 +1,48 @@
-export const Transfer = () => {
+import { Account } from "@polkadot-api/legacy-polkadot-provider"
+import { FormEventHandler, FunctionComponent, useEffect, useState } from "react"
+import Select from "react-select"
+
+export type Props = {
+  accounts: Account[]
+}
+
+export const Transfer: FunctionComponent<Props> = ({ accounts }) => {
   // TODO: handle form fields and submission with react
-  // TODO: fetch accounts from extension
   // TODO: fetch selected account balance
   // TODO: validate destination address
   // TODO: use PAPI to encode the transaction calldata
   // TODO: transfer should trigger an extension popup that signs the transaction
   // TODO: extract transaction submission into a hook
   // TODO: follow transaction submission events until it is finalized
+
+  useEffect(() => {
+    console.log("accounts", accounts)
+  }, [accounts])
+
+  const options = accounts.map((account) => ({
+    value: account.address,
+    label: account.displayName,
+  }))
+  const [selectedOption, setSelectedOption] = useState<any>(null)
+
+  const onSubmit: FormEventHandler = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <article>
       <header>Transfer funds</header>
-      <form>
-        <select>
-          <option disabled selected>
-            Select Account...
-          </option>
-          <option>Account 1</option>
-          <option>Account 2</option>
-        </select>
-        <small>Balance: 123456789</small>
+      <form onSubmit={onSubmit}>
+        <Select
+          defaultValue={selectedOption}
+          onChange={setSelectedOption}
+          options={options}
+        />
+        {selectedOption && <small>Balance: 123456789</small>}
         <input placeholder="to"></input>
         <input type="number" placeholder="amount"></input>
         <footer>
-          <button>Transfer</button>
+          <button type="submit">Transfer</button>
         </footer>
       </form>
     </article>
