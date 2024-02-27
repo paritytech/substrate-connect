350cb17a6423047834704fbddfdb886cadaa5d16
Matias Volpe <kratico@users.noreply.github.com>
2024-02-27 11:07:47 -0300

    fix: playwright tests (#1891)
    * fix: filter rococo chainspecs
    
    * chore: re-enable playwright examples test

--- examples/light-client-extension-helpers-extension/src/background.ts
+++ examples/light-client-extension-helpers-extension/src/background.ts
@@ -5,7 +5,16 @@ import { smoldotClient } from "./background-smoldot.code-split"
 
 const { lightClientPageHelper, addOnAddChainByUserListener } = register({
   smoldotClient,
-  getWellKnownChainSpecs: async () => wellKnownChainSpecs,
+  getWellKnownChainSpecs: async () =>
+    Object.fromEntries(
+      Object.entries(wellKnownChainSpecs)
+        // FIXME: remove filter once https://github.com/smol-dot/smoldot/issues/1691 is fixed
+        .filter(
+          ([genesisHash]) =>
+            genesisHash !==
+            "0x6408de7737c59c238890533af25896a2c20608d8b380bb01029acb392781063e",
+        ),
+    ),
 })
 
 addOnAddChainByUserListener(async (inputChain, tabId) => {
