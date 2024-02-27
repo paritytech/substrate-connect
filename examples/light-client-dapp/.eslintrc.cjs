9454f92dc7fe5f6146d5252bdca62811d69dff2e
Ryan Lee <ryan@parity.io>
2024-02-26 23:11:39 -0500

    chore(lc-dapp): exempt underscore variables names from eslint (#1880)

--- examples/light-client-dapp/.eslintrc.cjs
+++ examples/light-client-dapp/.eslintrc.cjs
@@ -10,6 +10,18 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        args: "all",
+        argsIgnorePattern: "^_",
+        caughtErrors: "all",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
