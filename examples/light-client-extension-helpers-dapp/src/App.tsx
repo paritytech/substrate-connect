9151b522a29640799283a3c524042ddffcec9d17
Ryan Lee <ryan@parity.io>
2024-02-29 13:50:45 -0500

    test

--- examples/light-client-extension-helpers-dapp/src/App.tsx
+++ examples/light-client-extension-helpers-dapp/src/App.tsx
@@ -3,7 +3,13 @@ import { useLightClientProvider } from "./hooks/useProvider"
 
 function App() {
   const { provider } = useLightClientProvider("extension-unique-id")
-  if (!provider) return null
+  if (!provider) {
+    return (
+      <main className="container">
+        <h1>Extension Test DApp</h1>
+      </main>
+    )
+  }
 
   return <Chains provider={provider} />
 }
