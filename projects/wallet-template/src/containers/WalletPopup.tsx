96c8a86fc16472902a6fcf012b2d2c795f6bac4f
Matias <kratico@users.noreply.github.com>
2024-03-05 09:59:05 -0300

    feat: useSWR to simplify async data fetching

--- projects/wallet-template/src/containers/WalletPopup.tsx
+++ projects/wallet-template/src/containers/WalletPopup.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { UserSignedExtensions } from "@polkadot-api/tx-helper"
-import type { SignRequest } from "../background/types"
+import useSWR from "swr"
 import {
   DecodedCallData,
   UserSignedExtensionInputs,
@@ -8,29 +8,26 @@ import {
 import { rpc } from "./WalletPopup/api"
 
 export const WalletPopup = () => {
-  const [signRequest, setSignRequest] = useState<{
-    id: string
-    request: SignRequest
-  }>()
+  const signRequestId = getSignRequestId()
   const [userSignedExtensions, setUserSignedExtensions] = useState<
     Partial<UserSignedExtensions>
   >({})
-  useEffect(() => {
-    const init = async () => {
-      const signRequests = await rpc.client.getSignRequests()
-      // TODO: handle many signRequests in a single window
-      const id = getSignRequestId()
-      if (!id || !signRequests[id]) return window.close()
-      setSignRequest({ id, request: signRequests[id] })
-    }
-    init()
-  }, [])
-  if (!signRequest) return null
-  const { request } = signRequest
+  const {
+    data: signRequest,
+    error,
+    isLoading,
+  } = useSWR(signRequestId, getSignRequest)
+  if (!signRequestId) {
+    window.close()
+    return null
+  }
+  if (isLoading) return null
+  if (error) return <div>error fetching sign request: {signRequestId}</div>
+  const request = signRequest!
   return (
     <main className="w-[32rem] mx-auto px-6 py-8">
       <div>
-        <h1 className="text-3xl font-bold">Sign Request #{signRequest.id}</h1>
+        <h1 className="text-3xl font-bold">Sign Request #{signRequestId}</h1>
         <div className="my-4">
           <div className="my-2">
             <div className="text-xs font-semibold">Origin</div>
@@ -66,10 +63,7 @@ export const WalletPopup = () => {
         <div className="flex justify-center space-x-4">
           <button
             onClick={() =>
-              rpc.client.approveSignRequest(
-                signRequest.id,
-                userSignedExtensions,
-              )
+              rpc.client.approveSignRequest(signRequestId, userSignedExtensions)
             }
             className="py-1.5 px-8 mr-4 text-sm rounded border border-[#24cc85] text-[#24cc85] hover:text-white
                   hover:bg-[#24cc85]"
@@ -77,7 +71,7 @@ export const WalletPopup = () => {
             Approve
           </button>
           <button
-            onClick={() => rpc.client.cancelSignRequest(signRequest.id)}
+            onClick={() => rpc.client.cancelSignRequest(signRequestId)}
             className="py-1.5 px-8 text-sm rounded border border-[#24cc85] text-[#24cc85] hover:text-white
                   hover:bg-[#24cc85]"
           >
@@ -93,4 +87,11 @@ const getSignRequestId = () => {
   const match = window.location.hash.match(/^#\/sign-request\/(\d+)$/)
   if (!match) return null
   return match[1]
+}
+
+const getSignRequest = async (id: string) => {
+  const signRequests = await rpc.client.getSignRequests()
+  const signRequest = signRequests[id]
+  if (!signRequest) throw new Error(`unknown sign request: ${id}`)
+  return signRequest
 }
--- projects/wallet-template/src/containers/WalletPopup/components/DecodedCallData.tsx
+++ projects/wallet-template/src/containers/WalletPopup/components/DecodedCallData.tsx
@@ -1,11 +1,10 @@
-import {
-  DecodedCall,
-  type ComplexDecoded,
-  type Decoded,
-  type PrimitiveDecoded,
+import type {
+  ComplexDecoded,
+  Decoded,
+  PrimitiveDecoded,
 } from "@polkadot-api/metadata-builders"
 import { toHex } from "@polkadot-api/utils"
-import { useEffect, useState } from "react"
+import useSWR from "swr"
 import { decodeCallData } from "../api"
 
 const jsonStringify = (value: any) =>
@@ -20,19 +19,23 @@ type Props = {
   callData: string
 }
 export const DecodedCallData = ({ chainId, callData }: Props) => {
-  const [decodedCallData, setDecodedCallData] = useState<DecodedCall>()
-  useEffect(() => {
-    decodeCallData(chainId, callData).then(setDecodedCallData)
-  }, [chainId, callData])
-  if (!decodedCallData) return <div>decoding...</div>
+  const {
+    data: decodedCallData,
+    isLoading,
+    error,
+  } = useSWR([chainId, callData], ([chainId, callData]) =>
+    decodeCallData(chainId, callData),
+  )
+  if (error) return <div>error decoding {callData}</div>
+  if (isLoading) return <div>decoding...</div>
   return (
     <div>
       <div className="text-xs font-semibold">Pallet Call</div>
       <div className="px-2">
-        {decodedCallData.pallet.value.name} {decodedCallData.call.value.name}
+        {decodedCallData!.pallet.value.name} {decodedCallData!.call.value.name}
       </div>
       <div className="text-xs font-semibold">Args</div>
-      <DecodedValue value={decodedCallData.args.value} />
+      <DecodedValue value={decodedCallData!.args.value} />
     </div>
   )
 }
