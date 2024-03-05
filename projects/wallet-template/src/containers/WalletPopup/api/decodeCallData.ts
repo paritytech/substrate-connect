0e319515983a330a2c094d9048ec271a6953da6a
Matias Volpe <kratico@users.noreply.github.com>
2024-03-05 16:47:55 -0300

    feat(wallet-template): add api to wallet popup (#1940)
    * feat(wallet-template): add api
    
    * feat(wallet-template): remove unused hooks
    
    * feat: useSWR to simplify async data fetching
    
    * feat: improve decodeCallData

---
+++ projects/wallet-template/src/containers/WalletPopup/api/decodeCallData.ts
@@ -0,0 +1,20 @@
+import { getObservableClient } from "@polkadot-api/client"
+import { getViewBuilder } from "@polkadot-api/metadata-builders"
+import { createClient } from "@polkadot-api/substrate-client"
+import { helper } from "@substrate/light-client-extension-helpers/extension-page"
+import { filter, firstValueFrom } from "rxjs"
+
+export const decodeCallData = async (chainId: string, callData: string) => {
+  const chains = await helper.getChains()
+  const chain = chains.find(({ genesisHash }) => genesisHash === chainId)
+  if (!chain) throw new Error("unkonwn chain")
+  const client = getObservableClient(createClient(chain.provider))
+  const { metadata$, unfollow } = client.chainHead$()
+  try {
+    const metadata = await firstValueFrom(metadata$.pipe(filter(Boolean)))
+    return getViewBuilder(metadata).callDecoder(callData)
+  } finally {
+    unfollow()
+    client.destroy()
+  }
+}
