7d264b40ddb316283165fad8ceb787fab71ae0cd
Ryan Lee <ryan@parity.io>
2024-03-05 10:50:54 -0500

    chore: well known system chains

--- packages/connect-known-chains/src/index.ts
+++ packages/connect-known-chains/src/index.ts
@@ -2,3 +2,16 @@ export { chainSpec as polkadot } from "./specs/polkadot"
 export { chainSpec as ksmcc3 } from "./specs/ksmcc3"
 export { chainSpec as westend2 } from "./specs/westend2"
 export { chainSpec as rococo_v2_2 } from "./specs/rococo_v2_2"
+
+export { chainSpec as polkadotAssetHub } from "./specs/asset-hub-polkadot"
+export { chainSpec as kusamaAssetHub } from "./specs/asset-hub-kusama"
+export { chainSpec as rococoAssetHub } from "./specs/asset-hub-rococo"
+export { chainSpec as westendAssetHub } from "./specs/asset-hub-westend"
+
+export { chainSpec as polkadotBridgeHub } from "./specs/bridge-hub-polkadot"
+export { chainSpec as kusamaBridgeHub } from "./specs/bridge-hub-kusama"
+export { chainSpec as rococoBridgeHub } from "./specs/bridge-hub-rococo"
+export { chainSpec as westendBridgeHub } from "./specs/bridge-hub-westend"
+
+export { chainSpec as polkadotCollectives } from "./specs/collectives-polkadot"
+export { chainSpec as westendCollectives } from "./specs/collectives-westend"
