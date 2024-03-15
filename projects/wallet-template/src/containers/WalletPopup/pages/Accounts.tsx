1ee061f43cba68be20bca43b9c2bd1682bbbcf3f
Ryan Lee <ryan@parity.io>
2024-03-15 17:25:40 -0400

    extract components

--- projects/wallet-template/src/containers/WalletPopup/pages/Accounts.tsx
+++ projects/wallet-template/src/containers/WalletPopup/pages/Accounts.tsx
@@ -7,10 +7,106 @@ import {
   PlusCircle,
 } from "lucide-react"
 import { Link, useNavigate } from "react-router-dom"
-import { ButtonHTMLAttributes } from "react"
 import { ss58Address } from "@polkadot-labs/hdkd-helpers"
 import { rpc } from "../api"
 import useSWR from "swr"
+import { IconButton } from "../../../components"
+import { Keyset } from "../../../background/types"
+
+type AccountItemProps = {
+  bgColor: string
+  text: string
+  subText: string
+}
+
+const AccountItem: React.FC<AccountItemProps> = ({
+  bgColor,
+  text,
+  subText,
+}) => {
+  const maxLength = 20 // Maximum length before adding ellipsis
+  const ellipsisText =
+    subText.length > maxLength
+      ? `${subText.substring(0, maxLength / 2)}...${subText.substring(subText.length - maxLength / 2, subText.length)}`
+      : subText
+
+  return (
+    <div className="flex items-center px-4 py-2 border-b">
+      <div
+        className={`flex-shrink-0 w-10 h-10 rounded-full ${bgColor} mr-3`}
+      ></div>
+      <div className="flex-grow">
+        <div className="text-sm font-medium">{text}</div>
+        <div className="text-xs text-gray-500">{ellipsisText}</div>
+      </div>
+      <ChevronRight className="text-gray-400" />
+    </div>
+  )
+}
+
+const EmptyAccounts: React.FC = () => {
+  return (
+    <section>
+      <div className="text-center">
+        <div className="mb-6">
+          <PlusCircle className="w-24 h-24 mx-auto text-gray-400" />
+        </div>
+        <h1 className="mb-2 text-xl font-semibold text-gray-700">
+          No Accounts Added
+        </h1>
+        <p className="mb-4 text-gray-600">
+          You haven't added any accounts yet. Let's get started.
+        </p>
+        <Link to={"/accounts/add"}>
+          <button
+            type="button"
+            className="inline-flex items-center justify-center px-4 py-2 text-white bg-teal-600 border border-transparent rounded-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+          >
+            <PlusCircle className="w-5 h-5 mr-2" />
+            Add Account
+          </button>
+        </Link>
+      </div>
+    </section>
+  )
+}
+
+type AccountsListProps = {
+  keysets: Record<string, Keyset>
+}
+
+const AccountsList: React.FC<AccountsListProps> = ({ keysets }) => {
+  // TODO: use mutliple keysets
+  const [keysetName, keyset] = Object.entries(keysets)[0]
+  const keypairs = keyset.derivationPaths.map(
+    ({ path, publicKey }) => [path, ss58Address(publicKey)] as const,
+  )
+
+  return (
+    <section>
+      <div className="flex flex-col items-center px-4 py-4">
+        <img
+          src="https://i.imgur.com/lp2ruGY.png"
+          alt="Profile"
+          className="w-24 h-24 rounded-full"
+        />
+        <h1 className="text-2xl font-bold mt-2">{keysetName}</h1>
+      </div>
+      <div className="px-4">
+        <h2 className="text-lg font-semibold mb-2">Derived Keys</h2>
+        <div className="bg-white rounded-lg shadow">
+          {keypairs.map(([path, ss58Address]) => (
+            <AccountItem
+              bgColor="bg-purple-200"
+              text={path}
+              subText={ss58Address}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
 
 export const Accounts = () => {
   const navigate = useNavigate()
@@ -22,109 +118,6 @@ export const Accounts = () => {
   const reset = async () => {
     await rpc.client.clearKeysets()
     navigate(0)
-  }
-
-  const IconButton: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
-    onClick,
-    children,
-    className,
-  }) => (
-    <button
-      onClick={onClick}
-      className={`text-gray-600 mx-2 hover:text-gray-500 ${className}`}
-    >
-      {children}
-    </button>
-  )
-
-  type AccountItemProps = {
-    bgColor: string
-    text: string
-    subText: string
-  }
-
-  const AccountItem: React.FC<AccountItemProps> = ({
-    bgColor,
-    text,
-    subText,
-  }) => {
-    const maxLength = 20 // Maximum length before adding ellipsis
-    const ellipsisText =
-      subText.length > maxLength
-        ? `${subText.substring(0, maxLength / 2)}...${subText.substring(subText.length - maxLength / 2, subText.length)}`
-        : subText
-
-    return (
-      <div className="flex items-center px-4 py-2 border-b">
-        <div
-          className={`flex-shrink-0 w-10 h-10 rounded-full ${bgColor} mr-3`}
-        ></div>
-        <div className="flex-grow">
-          <div className="text-sm font-medium">{text}</div>
-          <div className="text-xs text-gray-500">{ellipsisText}</div>
-        </div>
-        <ChevronRight className="text-gray-400" />
-      </div>
-    )
-  }
-  const EmptyAccounts = () => {
-    return (
-      <section>
-        <div className="text-center">
-          <div className="mb-6">
-            <PlusCircle className="w-24 h-24 mx-auto text-gray-400" />
-          </div>
-          <h1 className="mb-2 text-xl font-semibold text-gray-700">
-            No Accounts Added
-          </h1>
-          <p className="mb-4 text-gray-600">
-            You haven't added any accounts yet. Let's get started.
-          </p>
-          <Link to={"/accounts/add"}>
-            <button
-              type="button"
-              className="inline-flex items-center justify-center px-4 py-2 text-white bg-teal-600 border border-transparent rounded-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
-            >
-              <PlusCircle className="w-5 h-5 mr-2" />
-              Add Account
-            </button>
-          </Link>
-        </div>
-      </section>
-    )
-  }
-
-  const Accounts = () => {
-    // TODO: use mutliple keysets
-    const [keysetName, keyset] = Object.entries(keysets!)[0]
-    const keypairs = keyset.derivationPaths.map(
-      ({ path, publicKey }) => [path, ss58Address(publicKey)] as const,
-    )
-
-    return (
-      <section>
-        <div className="flex flex-col items-center px-4 py-4">
-          <img
-            src="https://i.imgur.com/lp2ruGY.png"
-            alt="Profile"
-            className="w-24 h-24 rounded-full"
-          />
-          <h1 className="text-2xl font-bold mt-2">{keysetName}</h1>
-        </div>
-        <div className="px-4">
-          <h2 className="text-lg font-semibold mb-2">Derived Keys</h2>
-          <div className="bg-white rounded-lg shadow">
-            {keypairs.map(([path, ss58Address]) => (
-              <AccountItem
-                bgColor="bg-purple-200"
-                text={path}
-                subText={ss58Address}
-              />
-            ))}
-          </div>
-        </div>
-      </section>
-    )
   }
 
   return (
@@ -150,7 +143,11 @@ export const Accounts = () => {
             </div>
           </div>
         </div>
-        {keysetsLength > 0 ? <Accounts /> : <EmptyAccounts />}
+        {keysetsLength > 0 ? (
+          <AccountsList keysets={keysets ?? {}} />
+        ) : (
+          <EmptyAccounts />
+        )}
       </div>
     </main>
   )
