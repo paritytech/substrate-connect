d4adf4764b6c45595af29358daf02686a390ad64
Ryan Lee <ryan@parity.io>
2024-04-02 10:04:43 -0400

    chore(wallet-template): accounts styling (#2042)

--- projects/wallet-template/src/containers/WalletPopup/pages/Accounts/SwitchAccount.tsx
+++ projects/wallet-template/src/containers/WalletPopup/pages/Accounts/SwitchAccount.tsx
@@ -40,44 +40,42 @@ export const SwitchAccount: React.FC = () => {
 
   const keysetNames = keysets?.map(({ name }) => name) ?? []
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="p-4">
-      <div className="max-w-xl p-6 mx-auto bg-white rounded-lg shadow-lg">
-        <h1 className="mb-4 text-2xl font-bold">Switch Account</h1>
-        <RadioGroup value={selectedKeysetName} onChange={setSelectedKeysetName}>
-          <RadioGroup.Label className="sr-only">Keyset</RadioGroup.Label>
-          <div className="space-y-2">
-            {keysetNames.map((keysetName) => (
-              <RadioGroup.Option
-                key={keysetName}
-                value={keysetName}
-                className={({ active, checked }) =>
-                  `${
-                    active
-                      ? "ring-2 ring-white/60 ring-offset-2 ring-offset-black"
-                      : ""
-                  }
+    <form className="max-w-xl p-6 mx-auto" onSubmit={handleSubmit(onSubmit)}>
+      <h1 className="mb-4 text-2xl font-bold">Switch Account</h1>
+      <RadioGroup value={selectedKeysetName} onChange={setSelectedKeysetName}>
+        <RadioGroup.Label className="sr-only">Keyset</RadioGroup.Label>
+        <div className="space-y-2">
+          {keysetNames.map((keysetName) => (
+            <RadioGroup.Option
+              key={keysetName}
+              value={keysetName}
+              className={({ active, checked }) =>
+                `${
+                  active
+                    ? "ring-2 ring-white/60 ring-offset-2 ring-offset-black"
+                    : ""
+                }
                   ${checked ? "bg-black/75 text-white" : "bg-white"}
                     relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none`
-                }
-              >
-                <div className="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center mr-3">
-                  <User className="text-gray-600" />
-                </div>
-                <div className="flex items-center justify-center">
-                  <span className="text-lg">{keysetName}</span>
-                </div>
-              </RadioGroup.Option>
-            ))}
-          </div>
-        </RadioGroup>
-        <div className="mt-6">
-          <button
-            type="submit"
-            className="w-full bg-teal-500 text-white py-2 rounded-lg hover:bg-teal-600 transition-colors"
-          >
-            Switch
-          </button>
+              }
+            >
+              <div className="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center mr-3">
+                <User className="text-gray-600" />
+              </div>
+              <div className="flex items-center justify-center">
+                <span className="text-lg">{keysetName}</span>
+              </div>
+            </RadioGroup.Option>
+          ))}
         </div>
+      </RadioGroup>
+      <div className="mt-6">
+        <button
+          type="submit"
+          className="w-full bg-teal-500 text-white py-2 rounded-lg hover:bg-teal-600 transition-colors"
+        >
+          Switch
+        </button>
       </div>
     </form>
   )
