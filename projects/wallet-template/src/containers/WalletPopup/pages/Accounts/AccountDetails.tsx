8d6e207bd35be065d6b2ac1fc8a31418ff139677
Ryan Lee <ryan@parity.io>
2024-04-16 22:25:30 -0400

    better

--- projects/wallet-template/src/containers/WalletPopup/pages/Accounts/AccountDetails.tsx
+++ projects/wallet-template/src/containers/WalletPopup/pages/Accounts/AccountDetails.tsx
@@ -1,23 +1,65 @@
-import { Globe, Clipboard } from "lucide-react"
-import React from "react"
+import { Globe, Clipboard, CheckCircle, ArrowLeft } from "lucide-react"
+import React, { useState } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+import { useCopyToClipboard } from "usehooks-ts"
 
 export const AccountDetails: React.FC = () => {
+  const navigate = useNavigate()
+  const { accountId } = useParams<{ accountId: string }>()
+  const [copied, setCopied] = useState(false)
+  const [_, copy] = useCopyToClipboard()
+
+  if (!accountId) {
+    return null
+  }
+
+  const onCopyToClipboard = async () => {
+    setCopied(true)
+    setTimeout(() => {
+      setCopied(false)
+    }, 2000)
+    await copy(accountId)
+  }
+
   return (
     <section
       aria-label="Account Details"
       className="mb-8 mx-auto bg-white shadow rounded-lg"
     >
-      <h1 className="text-3xl font-semibold text-center p-4 border-b">
-        Account Details
-      </h1>
+      <section className="text-center w-full max-w-lg">
+        <button
+          className="flex items-center font-semibold"
+          onClick={() => navigate(-1)}
+        >
+          <ArrowLeft className="mr-2" /> Go Back
+        </button>
+        <h1 className="text-3xl font-semibold text-center p-4 border-b">
+          Account Details
+        </h1>
+      </section>
 
       <div className="p-6">
         <div className="flex justify-between items-center mb-6">
           <h2 className="text-xl font-semibold">Wallet Address</h2>
-          <Clipboard className="cursor-pointer text-gray-500 hover:text-gray-700" />
+          <button
+            type="button"
+            className={`p-2 rounded flex items-center justify-center space-x-2 ${copied ? "bg-green-200" : ""}`}
+            aria-label="Copy to clipboard"
+            onClick={onCopyToClipboard}
+          >
+            {copied ? (
+              <div className="flex items-center space-x-2">
+                <CheckCircle className="text-green-500" size={24} />
+              </div>
+            ) : (
+              <div className="flex items-center space-x-2">
+                <Clipboard size={24} />
+              </div>
+            )}
+          </button>
         </div>
-        <p className="text-sm mb-4 bg-gray-200 p-3 rounded text-center">
-          5FFMCdRt2GsEcrG5WJCKtXQ8nnpLTe3jrio3YS3JFeS5vzJb
+        <p className="text-xs mb-4 bg-gray-200 p-3 rounded text-center">
+          {accountId?.toUpperCase()}
         </p>
 
         <div className="space-y-4">
