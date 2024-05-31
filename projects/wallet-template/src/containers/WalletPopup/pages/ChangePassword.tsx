aa69fcb678dc38518be9224ab83d77186cd358a2
Ryan Lee <ryan@parity.io>
2024-05-31 11:50:37 -0400

    chore(wallet-template): update confirm password (#2209)

--- projects/wallet-template/src/containers/WalletPopup/pages/ChangePassword.tsx
+++ projects/wallet-template/src/containers/WalletPopup/pages/ChangePassword.tsx
@@ -1,113 +1,230 @@
 import { useForm, SubmitHandler } from "react-hook-form"
 import { rpc } from "../api"
-import { Layout } from "../../../components/Layout"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Layout2 } from "@/components/Layout2"
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+} from "@/components/ui/card"
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { ArrowLeft, EyeOffIcon, EyeIcon } from "lucide-react"
+import { cn } from "@/lib/utils"
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+import { Header } from "../components"
 
-type FormFields = {
-  currentPassword: string
-  newPassword: string
-  newPasswordConfirm: string
-}
+const changePasswordSchema = z
+  .object({
+    currentPassword: z.string().min(1, "Current password is required"),
+    newPassword: z
+      .string()
+      .min(6, "New password must be at least 6 characters long"),
+    newPasswordConfirm: z.string(),
+  })
+  .refine((data) => data.newPassword === data.newPasswordConfirm, {
+    message: "New password and confirm new password must match",
+    path: ["newPasswordConfirm"],
+  })
+
+type FormFields = z.infer<typeof changePasswordSchema>
 
 export const ChangePassword = () => {
+  const navigate = useNavigate()
+
+  const [showCurrentPassword, setShowCurrentPassword] = useState(false)
+  const [showNewPassword, setShowNewPassword] = useState(false)
+  const [showConfirmNewPassword, setShowConfirmNewPassword] = useState(false)
+
+  const form = useForm<FormFields>({
+    resolver: zodResolver(changePasswordSchema),
+  })
   const {
-    register,
     handleSubmit,
-    setError,
     reset,
-    formState: { isSubmitting, errors, isSubmitSuccessful },
-    watch,
-  } = useForm<FormFields>()
+    formState: { isSubmitting },
+    control,
+    setError,
+  } = form
+
   const onSubmit: SubmitHandler<FormFields> = async ({
     currentPassword,
     newPassword,
   }) => {
-    await rpc.client.changePassword(currentPassword, newPassword)
-    reset()
+    try {
+      await rpc.client.changePassword(currentPassword, newPassword)
+      reset()
+      navigate("/debug")
+    } catch {
+      setError("currentPassword", {
+        type: "server",
+        message: "Current password is incorrect",
+      })
+    }
   }
+
   return (
-    <Layout>
-      <div className="my-4">
-        <h1 className="text-3xl font-bold text-center">Change Password</h1>
-      </div>
-      <form
-        onSubmit={(e) =>
-          handleSubmit(onSubmit)(e).catch((e) =>
-            setError("root", {
-              message: e.message ?? "Error updating password",
-            }),
-          )
-        }
-      >
-        <div className="my-4">
-          <input
-            type="password"
-            placeholder="Enter old password"
-            className="w-full p-3 text-lg border rounded"
-            autoFocus
-            {...register("currentPassword", {
-              required: "You must specify a password",
-              minLength: {
-                value: 6,
-                message: "Password must have at least 6 characters",
-              },
-            })}
-          />
-          {errors.currentPassword && (
-            <div className="text-red-500">{errors.currentPassword.message}</div>
-          )}
-        </div>
-        <div className="my-4">
-          <input
-            type="password"
-            placeholder="Enter new password"
-            className="w-full p-3 text-lg border rounded"
-            {...register("newPassword", {
-              required: "You must specify a password",
-              minLength: {
-                value: 6,
-                message: "Password must have at least 6 characters",
-              },
-            })}
-          />
-          {errors.newPassword && (
-            <div className="text-red-500">{errors.newPassword.message}</div>
-          )}
-        </div>
-        <div className="my-4">
-          <input
-            type="password"
-            placeholder="Re-enter new password"
-            className="w-full p-3 text-lg border rounded"
-            {...register("newPasswordConfirm", {
-              required: "You must confirm the password",
-              validate: (value) =>
-                value === watch("newPassword") || "The passwords do not match",
-            })}
-          />
-          {errors.newPasswordConfirm && (
-            <div className="text-red-500">
-              {errors.newPasswordConfirm.message}
-            </div>
-          )}
-        </div>
-        <div className="my-4">
-          <button
-            type="submit"
-            disabled={isSubmitting}
-            className="w-full p-3 text-lg rounded border border-[#24cc85] text-[#24cc85] hover:text-white hover:bg-[#24cc85]"
-          >
-            {isSubmitting ? "Changing password..." : "Change password"}
-          </button>
-          <div className="mt-2 text-red-500">
-            {errors.root && (
-              <div className="text-red-500">{errors.root.message}</div>
-            )}
-          </div>
-        </div>
-        {isSubmitSuccessful && (
-          <div className="my-4">Password updated successfully</div>
-        )}
-      </form>
-    </Layout>
+    <Layout2>
+      <Header />
+      <Form {...form}>
+        <Card className="flex flex-col flex-grow w-full max-w-md shadow-none">
+          <CardHeader>
+            <CardTitle>Change Password</CardTitle>
+            <CardDescription>
+              Use this form to securely update your password.
+            </CardDescription>
+          </CardHeader>
+          <form className="relative grow" onSubmit={handleSubmit(onSubmit)}>
+            <CardContent className="content-center flex-grow">
+              <FormField
+                control={control}
+                name="currentPassword"
+                render={({ field }) => (
+                  <FormItem className="grid gap-2 mb-4">
+                    <FormLabel>Current Password</FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <Input
+                          {...field}
+                          type={showCurrentPassword ? "text" : "password"}
+                          className="pr-10"
+                        />
+                        <Button
+                          type="button"
+                          onClick={() =>
+                            setShowCurrentPassword(!showCurrentPassword)
+                          }
+                          className="absolute inset-y-0 right-0 flex items-center pr-3"
+                          variant="ghost"
+                          size="sm"
+                          aria-label="Toggle current password visibility"
+                        >
+                          {showCurrentPassword ? (
+                            <EyeOffIcon className="w-4 h-4" />
+                          ) : (
+                            <EyeIcon className="w-4 h-4" />
+                          )}
+                        </Button>
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={control}
+                name="newPassword"
+                render={({ field }) => (
+                  <FormItem className="grid gap-2 mb-4">
+                    <FormLabel>New Password</FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <Input
+                          {...field}
+                          type={showNewPassword ? "text" : "password"}
+                          className="pr-10"
+                        />
+                        <Button
+                          type="button"
+                          onClick={() => setShowNewPassword(!showNewPassword)}
+                          className="absolute inset-y-0 right-0 flex items-center pr-3"
+                          variant="ghost"
+                          size="sm"
+                          aria-label="Toggle new password visibility"
+                        >
+                          {showCurrentPassword ? (
+                            <EyeOffIcon className="w-4 h-4" />
+                          ) : (
+                            <EyeIcon className="w-4 h-4" />
+                          )}
+                        </Button>
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={control}
+                name="newPasswordConfirm"
+                render={({ field }) => (
+                  <FormItem className="grid gap-2 mb-4">
+                    <FormLabel>Confirm New Password</FormLabel>
+                    <FormControl>
+                      <div className="relative">
+                        <Input
+                          {...field}
+                          type={showConfirmNewPassword ? "text" : "password"}
+                          className="pr-10"
+                        />
+                        <Button
+                          type="button"
+                          onClick={() =>
+                            setShowConfirmNewPassword(!showConfirmNewPassword)
+                          }
+                          className="absolute inset-y-0 right-0 flex items-center pr-3"
+                          variant="ghost"
+                          size="sm"
+                          aria-label="Toggle confirm new password visibility"
+                        >
+                          {showCurrentPassword ? (
+                            <EyeOffIcon className="w-4 h-4" />
+                          ) : (
+                            <EyeIcon className="w-4 h-4" />
+                          )}
+                        </Button>
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter className="flex flex-col flex-grow space-y-4">
+              <Button
+                type="submit"
+                className={cn(
+                  "w-full py-3",
+                  "text-lg text-background",
+                  "bg-emerald-600 hover:bg-emerald-700",
+                )}
+              >
+                Submit
+              </Button>
+              <Button
+                type="button"
+                disabled={isSubmitting}
+                autoFocus
+                className={cn(
+                  "w-full py-3",
+                  "text-lg text-emerald-700",
+                  "bg-emerald-100 hover:bg-emerald-200",
+                )}
+                onClick={() => navigate("/debug")}
+              >
+                <ArrowLeft
+                  className="w-5 h-5 mr-2 text-emerald-500"
+                  aria-hidden="true"
+                />
+                Go Back
+              </Button>
+            </CardFooter>
+          </form>
+        </Card>
+      </Form>
+    </Layout2>
   )
 }
