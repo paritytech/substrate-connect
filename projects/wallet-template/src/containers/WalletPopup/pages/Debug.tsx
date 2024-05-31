064979bea62f035921f17868b66a9775d5bc25a0
Ryan Lee <ryan@parity.io>
2024-05-30 23:40:36 -0400

    chore(wallet-template): update debug page

--- projects/wallet-template/src/containers/WalletPopup/pages/Debug.tsx
+++ projects/wallet-template/src/containers/WalletPopup/pages/Debug.tsx
@@ -1,34 +1,37 @@
 import { Link } from "react-router-dom"
 import { useKeyring } from "../hooks"
-import { Layout } from "../../../components/Layout"
+import { Layout2 } from "@/components/Layout2"
+import { BottomNavBar, Header } from "../components"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Button } from "@/components/ui/button"
+import { KeyIcon, LockIcon } from "lucide-react"
 
-export const Debug = () => {
+export const Debug: React.FC = () => {
   const { lock } = useKeyring()
+
   return (
-    <Layout>
-      <h1 className="text-3xl font-bold">Wallet</h1>
-      <div className="my-4 flex space-x-4">
-        <Link
-          to={"/accounts"}
-          className="py-1.5 px-8 text-sm rounded border border-[#24cc85] text-[#24cc85] hover:text-white hover:bg-[#24cc85]"
-        >
-          Accounts
-        </Link>
+    <Layout2>
+      <Header />
+      <div className="flex items-center justify-between px-6 mt-4 mb-4 sm:px-8">
+        <h2 className="text-xl font-semibold">Debug</h2>
       </div>
-      <div className="my-4 flex space-x-4">
-        <Link
-          to={"/change-password"}
-          className="py-1.5 px-8 text-sm rounded border border-[#24cc85] text-[#24cc85] hover:text-white hover:bg-[#24cc85]"
-        >
-          Change Password
-        </Link>
-        <button
-          onClick={() => lock()}
-          className="py-1.5 px-8 text-sm rounded border border-[#24cc85] text-[#24cc85] hover:text-white hover:bg-[#24cc85]"
-        >
-          Lock
-        </button>
-      </div>
-    </Layout>
+      <ScrollArea className="px-6 mb-4 grow sm:px-8">
+        <div className="flex flex-col space-y-4">
+          <Button variant="outline" onClick={() => lock()}>
+            <LockIcon className="w-4 h-4 mr-2 text-muted-foreground" />
+            Lock Wallet
+          </Button>
+          <Button variant="outline" asChild>
+            <Link to={"/change-password"}>
+              <>
+                <KeyIcon className="w-4 h-4 mr-2 text-muted-foreground" />
+                Change Password
+              </>
+            </Link>
+          </Button>
+        </div>
+      </ScrollArea>
+      <BottomNavBar currentItem="debug" />
+    </Layout2>
   )
 }
