0ce9864b5f87cb413eacb234ea7fc2a619d3d74e
Ryan Lee <ryan@parity.io>
2024-07-25 10:18:17 -0400

    refactor(smoldot-discovery): simplify isSmoldotExtension (#2378)

---
+++ .changeset/pretty-eels-sit.md
@@ -0,0 +1,5 @@
+---
+"@substrate/smoldot-discovery": patch
+---
+
+simplify isSmoldotExtension
--- packages/smoldot-discovery/src/index.ts
+++ packages/smoldot-discovery/src/index.ts
@@ -5,11 +5,7 @@ export const Kind: SmoldotExtensionProviderDetail["kind"] = "smoldot-v1"
 
 export const isSmoldotExtension = (
   provider: ProviderDetail,
-): provider is SmoldotExtensionProviderDetail => {
-  if (provider.kind !== Kind) return false
-
-  return true
-}
+): provider is SmoldotExtensionProviderDetail => provider.kind === Kind
 
 export const getSmoldotExtensionProviders = () => {
   return getProviders().filter(isSmoldotExtension)
