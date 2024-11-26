cb1b0519ffd7c874d993ca2180afe02a511536fd
Ryan Lee <ryan@parity.io>
2024-11-26 02:55:02 -0500

    fix: smoldot discovery connector webpack bundler issues (#2569)

---
+++ packages/smoldot-discovery-connector/tsup.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig([
+  {
+    entry: {
+      index: "src/index.ts",
+    },
+    outDir: "dist/esm",
+    format: ["esm"],
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    tsconfig: "./tsconfig.build.json",
+    noExternal: ["effect"],
+  },
+  {
+    entry: {
+      index: "src/index.ts",
+    },
+    outDir: "dist/cjs",
+    format: ["cjs"],
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    tsconfig: "./tsconfig.build.json",
+    noExternal: ["effect"],
+  },
+])
